6b03ad8943ec6c069df863113c7bc239f6d99d7d
touchty <touchxyz@163.com>
2018-04-03 23:25:05 +0800

    push from idea leetcode project

---
+++ src/WildcardMatching.java
@@ -0,0 +1,52 @@
+public class WildcardMatching {
+    public boolean isMatch(String s, String p) {
+        boolean[][] match=new boolean[s.length()+1][p.length()+1];
+        match[s.length()][p.length()]=true;
+        for(int i=p.length()-1;i>=0;i--){
+            if(p.charAt(i)!='*')
+                break;
+            else
+                match[s.length()][i]=true;
+        }
+        for(int i=s.length()-1;i>=0;i--){
+            for(int j=p.length()-1;j>=0;j--){
+                if(s.charAt(i)==p.charAt(j)||p.charAt(j)=='?')
+                    match[i][j]=match[i+1][j+1];
+                else if(p.charAt(j)=='*')
+                    match[i][j]=match[i+1][j]||match[i][j+1];
+                else
+                    match[i][j]=false;
+            }
+        }
+        return match[0][0];
+    }
+
+    public boolean isMatch_2d_method(String s, String p) {
+        int m=s.length(), n=p.length();
+        boolean[][] dp = new boolean[m+1][n+1];
+        dp[0][0] = true;
+        for (int i=1; i<=m; i++) {
+            dp[i][0] = false;
+        }
+
+        for(int j=1; j<=n; j++) {
+            if(p.charAt(j-1)=='*'){
+                dp[0][j] = true;
+            } else {
+                break;
+            }
+        }
+
+        for(int i=1; i<=m; i++) {
+            for(int j=1; j<=n; j++) {
+                if (p.charAt(j-1)!='*') {
+                    dp[i][j] = dp[i-1][j-1] && (s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1)=='?');
+                } else {
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
+
