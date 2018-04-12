e0ed14ddb8d474e736b9ba54e489d9467f5ef39a
touchty <touchxyz@163.com>
2018-04-12 19:01:07 +0800

    push from idea leetcode project

--- src/dp/RangeSumQuery2D.java
+++ src/dp/RangeSumQuery2D.java
@@ -1,5 +1,7 @@
 package dp;
 
+import javafx.scene.transform.MatrixType;
+
 public class RangeSumQuery2D {
     private int[][] dp;
 
@@ -30,4 +32,37 @@ public class RangeSumQuery2D {
 
         return dp[iMax + 1][jMax + 1] - dp[iMax + 1][jMin] - dp[iMin][jMax + 1] + dp[iMin][jMin];
     }
+
+    public void RangeSumQuery2DRewrite(int[][] matrix){
+        if (matrix == null ||
+                matrix.length == 0||
+                matrix[0].length == 0){
+            return;
+        }
+
+        //  mxn dimension
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[][] dp = new int[m+1][n+1];
+
+        for (int i = 1; i <= m; i++){
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1] + matrix[i-1][j-1];
+            }
+        }
+    }
+
+    public int sumRegionRewrite(int row1, int col1, int row2, int col2) {
+        int iMin = Math.min(row1, row2);
+        int iMax = Math.max(row1, row2);
+
+        int jMin = Math.min(col1, col2);
+        int jMax = Math.max(col1, col2);
+
+        int result = dp[iMax+1][jMax+1] - dp[iMax+1][jMin] - dp[iMin][jMax+1] +dp[iMin][jMin];
+
+        return result;
+    }
+
 }
--- test/dp/RangeSumQuery2DTest.java
+++ test/dp/RangeSumQuery2DTest.java
@@ -1,6 +1,7 @@
 package dp; 
 
-import org.junit.Test; 
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.Before; 
 import org.junit.After; 
 
@@ -28,9 +29,44 @@ public void after() throws Exception {
 */ 
 @Test
 public void testSumRegion() throws Exception { 
+    int[][] matrix={
+            {3, 0, 1, 4, 2},
+            {5, 6, 3, 2, 1},
+            {1, 2, 0, 1, 5},
+            {4, 1, 0, 1, 7},
+            {1, 0, 3, 0, 5}};
+    RangeSumQuery2D rq = new RangeSumQuery2D(matrix);
+    int result = rq.sumRegion(1, 2, 2, 4);
+    Assert.assertTrue(result == 12);
 
 }
 
+/** 
+* 
+* Method: RangeSumQuery2DRewrite(int[][] matrix) 
+* 
+*/ 
+@Test
+public void testRangeSumQuery2DRewrite() throws Exception { 
+}
+
+/** 
+* 
+* Method: sumRegionRewrite(int row1, int col1, int row2, int col2) 
+* 
+*/ 
+@Test
+public void testSumRegionRewrite() throws Exception {
+    int[][] matrix={
+            {3, 0, 1, 4, 2},
+            {5, 6, 3, 2, 1},
+            {1, 2, 0, 1, 5},
+            {4, 1, 0, 1, 7},
+            {1, 0, 3, 0, 5}};
+    RangeSumQuery2D rq = new RangeSumQuery2D(matrix);
+    int result = rq.sumRegionRewrite(1, 2, 2, 4);
+    Assert.assertTrue(result == 12);
+}
 
 
 } 
