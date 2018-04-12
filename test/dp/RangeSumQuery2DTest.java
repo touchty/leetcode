1a4aad689c891a6178fbff34231a7d1d67bc0df8
touchty <touchxyz@163.com>
2018-04-12 15:37:45 +0800

    push from idea leetcode project

---
+++ test/dp/RangeSumQuery2DTest.java
@@ -0,0 +1,36 @@
+package dp; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* RangeSumQuery2D Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Apr 12, 2018</pre> 
+* @version 1.0 
+*/ 
+public class RangeSumQuery2DTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: sumRegion(int row1, int col1, int row2, int col2) 
+* 
+*/ 
+@Test
+public void testSumRegion() throws Exception { 
+
+}
+
+
+
+} 
