ddd9b62d0de7e9270cefde768d5e99f5a4a73963
touchty <touchxyz@163.com>
2018-04-15 20:01:17 +0800

    push from idea leetcode project

---
+++ src/dp/ArithmeticSlices.java
@@ -0,0 +1,22 @@
+package dp;
+
+public class ArithmeticSlices {
+    public static int numberOfArithmeticSlices(int [] A){
+        int curr = 0;
+
+        int sum = 0;
+
+        for (int i = 2; i < A.length; i++) {
+            // curr = curr + 1
+            // curr == 0 means i-1, i-2, i-3 are not Arithmetic Sequences
+            // curr != 0 means i -1, i-2 i-3, ...are Arithmetic Sequences, and when i, i-1, i-2
+            // are Arithmetic Sequences, we increase curr by 1
+            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]){
+                curr += 1;
+                sum += curr;
+            }
+            else curr = 0;
+        }
+        return sum;
+    }
+}
