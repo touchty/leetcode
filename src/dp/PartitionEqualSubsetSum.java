79f240643f3c3fd0362ccfb14482826da3a33d54
touchty <touchxyz@163.com>
2018-04-15 21:18:47 +0800

    push from idea leetcode project

--- src/dp/PartitionEqualSubsetSum.java
+++ src/dp/PartitionEqualSubsetSum.java
@@ -43,4 +43,32 @@ public class PartitionEqualSubsetSum {
 
         return dp[n][sum];
     }
+
+    public boolean canPartition1D(int[] nums) {
+        int sum = 0;
+
+        for (int num : nums) {
+            sum += num;
+        }
+
+        if ((sum & 1) == 1) {
+            return false;
+        }
+        sum /= 2;
+
+        int n = nums.length;
+        boolean[] dp = new boolean[sum+1];
+        Arrays.fill(dp, false);
+        dp[0] = true;
+
+        for (int num : nums) {
+            for (int i = sum; i > 0; i--) {
+                if (i >= num) {
+                    dp[i] = dp[i] || dp[i-num];
+                }
+            }
+        }
+
+        return dp[sum];
+    }
 }
