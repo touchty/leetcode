c829292bac2867f8197ace3f44d29a28abfc394c
touchty <touchxyz@163.com>
2018-04-19 21:15:36 +0800

    push from idea leetcode project

---
+++ src/dp/PalindromicSubstrings.java
@@ -0,0 +1,29 @@
+package dp;
+
+public class PalindromicSubstrings {
+    int count = 0;
+
+    public int countSubstrings(String s){
+        if (s == null || s.length() == 0){
+            return 0;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            extendLen(s,i,i);
+            extendLen(s,i,i+1);
+        }
+        return count;
+    }
+
+    void extendLen(String s, int l, int r){
+        while (l >= 0 && r < s.length()){
+            if (s.charAt(l) == s.charAt(r)){
+                l--;
+                r++;
+                count++;
+            }
+            else
+                break;
+        }
+    }
+}
