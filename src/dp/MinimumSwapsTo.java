e7eab5a7892d1914c50f85ea5538a832945cf199
touchty <touchxyz@163.com>
2018-04-20 22:33:12 +0800

    push from idea leetcode project

---
+++ src/dp/MinimumSwapsTo.java
@@ -0,0 +1,35 @@
+package dp;
+
+import java.util.Arrays;
+
+public class MinimumSwapsTo {
+    public int minSwap(int[] A, int[] B) {
+        int n = A.length;
+        int[] swap = new int[n];
+        int[] no_swap = new int[n];
+        Arrays.fill(swap,n);
+        Arrays.fill(no_swap,n);
+        swap[0] = 1;
+        no_swap[0] = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (A[i] > A[i-1] && B[i] > B[i-1]){
+                // If we swap position i, we need to swap position i - 1.
+                swap[i] = swap[i - 1] + 1;
+
+                // If we don't swap position i , we should not swap position i - 1.
+                no_swap[i] = no_swap[i - 1];
+
+            }
+
+            if (A[i] > B[i-1] && B[i] > A[i-1]){
+                // If we swap position i, we should not swap position i - 1.
+                swap[i] = Math.min(swap[i], no_swap[i - 1] + 1);
+
+                // If we don't swap position i, we should swap position i - 1.
+                no_swap[i] = Math.min(no_swap[i], swap[i - 1]);
+            }
+        }
+        return Math.min(swap[n-1],no_swap[n-1]);
+    }
+}
---
+++ src/dp/OnesandZeroes.java
@@ -0,0 +1,32 @@
+package dp;
+
+public class OnesandZeroes {
+    // catch: // dp[i][j] = the max number of strings that can be formed with i 0's and j 1's
+    // from the first few strings up to the current string s
+    // Catch: have to go from bottom right to top left
+    // Why? If a cell in the memo is updated(because s is selected),
+    // we should be adding 1 to memo[i][j] from the previous iteration (when we were not considering s)
+    // If we go from top left to bottom right, we would be using results from this iteration => overcounting
+    // str can be used only once.
+    public int findMaxForm(String[] strs, int m, int n) {
+        int[][] dp = new int[m+1][n+1];
+
+        for (String str : strs){
+            int zeros = 0;
+            int ones = 0;
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == '0')
+                    zeros++;
+                else if (str.charAt(i) == '1')
+                    ones++;
+            }
+
+            for (int i = m; i >= zeros ; i--) {
+                for (int j = n; j >= ones ; j--) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i-zeros][j-ones] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
