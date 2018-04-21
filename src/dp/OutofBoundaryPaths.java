d644c71d9fff7d3a8b29b9f45d8e252c8de6817a
touchty <touchxyz@163.com>
2018-04-21 21:22:46 +0800

    push from idea leetcode project

--- src/dp/OutofBoundaryPaths.java
+++ src/dp/OutofBoundaryPaths.java
@@ -4,7 +4,7 @@ public class OutofBoundaryPaths {
     public int findPaths(int m, int n, int N, int i, int j) {
         if (N <= 0) return 0;
 
-        final int MOD = 1000000007;
+        final int MOD = 1_000_000_007;
         int[][] count = new int[m][n];
         count[i][j] = 1;
         int result = 0;
@@ -20,8 +20,7 @@ public class OutofBoundaryPaths {
                         int nc = c + d[1];
                         if (nr < 0 || nr >= m || nc < 0 || nc >= n) {
                             result = (result + count[r][c]) % MOD;
-                        }
-                        else {
+                        } else {
                             temp[nr][nc] = (temp[nr][nc] + count[r][c]) % MOD;
                         }
                     }
@@ -30,6 +29,40 @@ public class OutofBoundaryPaths {
             count = temp;
         }
 
+        return result;
+    }
+
+    public int findPathsRewrite(int m, int n, int N, int i, int j) {
+        if (N <= 0)
+            return 0;
+
+        final int MOD = 1_000_000_007;
+        // possible ways to reach (i,j)
+        int[][] count = new int[m][n];
+        count[i][j] = 1;
+        int result = 0;
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        for (int step = 0; step < N; step++){
+            int[][] temp = new int[m][n];
+            for (int r = 0; r < m; r++) {
+                for (int c = 0; c < n; c++) {
+                    for (int[] dir : dirs){
+                        // four directions
+                        int nr = r + dir[0];
+                        int nc = c + dir[1];
+
+                        if (nr <0 || nr>=m ||nc<0||nc>=n){
+                            result += count[r][c];
+                        }else {
+                            temp[nr][nc] = (temp[nr][nc] + count[r][c]) % MOD;
+                        }
+                    }
+                }
+
+            }
+            count=temp;
+        }
         return result;
     }
 }
