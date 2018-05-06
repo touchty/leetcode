acea30377a31b387a0ed02630871beb52be35385
touchty <touchxyz@163.com>
2018-05-07 00:28:30 +0800

    Merge branch 'master' of https://github.com/touchty/leetcode

---
+++ src/binarySearch/FindPeakElement.java
@@ -0,0 +1,47 @@
+package binarySearch;
+
+public class FindPeakElement {
+    public int findPeakElement(int[] nums) {
+        return search(nums, 0, nums.length - 1);
+    }
+    public int search(int[] nums, int l, int r) {
+        if (l == r)
+            return l;
+        int mid = (l + r) / 2;
+        if (nums[mid] > nums[mid + 1])
+            return search(nums, l, mid);
+        return search(nums, mid + 1, r);
+    }
+
+    public int findPeakElementOptimized(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            int mid = (r - l) / 2 + l;
+            if ((mid == 0 || nums[mid] > nums[mid - 1]) && (mid == nums.length - 1 || nums[mid] > nums[mid + 1])) return mid;
+            if (mid == 0 || nums[mid] > nums[mid - 1]) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return l;
+    }
+
+    int findPeakElementBSIteration(int[] nums)
+    {
+        int low = 0;
+        int high = nums.length-1;
+
+        while(low < high)
+        {
+            int mid1 = (low+high)/2;
+            int mid2 = mid1+1;
+            if(nums[mid1] < nums[mid2])
+                low = mid2;
+            else
+                high = mid1;
+        }
+        return low;
+    }
+}
---
+++ src/binarySearch/LoweCommonAncestorofaBinarySearchTree.java
@@ -0,0 +1,12 @@
+package binarySearch;
+
+
+public class LoweCommonAncestorofaBinarySearchTree {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while ((root.val - p.val) * (root.val - q.val) > 0)
+            root = p.val < root.val ? root.left : root.right;
+        return root;
+    }
+}
+
