1b0c4f5cffe8ca37b0db3992346324f57a3a3d73
touchty <touchxyz@163.com>
2018-05-07 10:41:09 +0800

    Merge branch 'master' of https://github.com/touchty/leetcode

---
+++ src/binarySearch/CountCompleteTreeNodes.java
@@ -0,0 +1,26 @@
+package binarySearch;
+
+public class CountCompleteTreeNodes {
+    int height(TreeNode root){
+        return root == null ? -1 : 1 + height(root.left);
+    }
+
+    public int countNodes(TreeNode root){
+        int h = height(root);
+
+        int count = 0;
+        while (root != null){
+            if (height(root.right) == h - 1){
+                count += 1 << h;  // left subtree
+                root = root.right;
+            }
+            else{
+                count += 1 << (h - 1); // right subtree
+                root = root.left;
+            }
+            h--;
+        }
+        return count;
+
+    }
+}
---
+++ src/binarySearch/FindMinimuminRotatedSortedArray.java
@@ -0,0 +1,17 @@
+package binarySearch;
+
+public class FindMinimuminRotatedSortedArray {
+    /*
+    * int[] nums = {7,8,9,10,1,2,3,4,5,6}; find disorder in right part;
+    * */
+    public int findMin(int[] nums) {
+        int lo = 0;
+        int hi = nums.length - 1;
+        while (lo < hi){
+            int mid = (lo + hi) / 2;
+            if (nums[mid] > nums[hi]) lo = mid + 1;
+            else hi = mid;
+        }
+        return nums[lo];
+    }
+}
