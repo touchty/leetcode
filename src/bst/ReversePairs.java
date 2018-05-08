63756e3021e138fcc3c0e06a473d570cc48f60b4
touchty <touchxyz@163.com>
2018-05-08 16:32:58 +0800

    Merge branch 'master' of https://github.com/touchty/leetcode

--- src/bst/ReversePairs.java
+++ src/bst/ReversePairs.java
@@ -59,7 +59,11 @@ public class ReversePairs {
     private int mergeSort(int[] nums, int s, int e){
         if(s>=e) return 0;
         int mid = s + (e-s)/2;
+        // after mergeSort1(nums, s, mid), nums[s...mid] is sorted.
+        // after mergeSort1(nums, mid+1,e), nums[mid+1...e] is sorted.
         int cnt = mergeSort(nums, s, mid) + mergeSort(nums, mid+1, e);
+
+        // compare between two sorted subarrays, position of i and j respectively
         for(int i = s, j = mid+1; i<=mid; i++){
             while(j<=e && nums[i]/2.0 > nums[j]) j++;
             cnt += j-(mid+1);
