87c562892cb9d7d100086c88ccec93861dc8e603
touchty <touchxyz@163.com>
2018-05-29 15:37:06 +0800

    idea

---
+++ src/bst/ReconstructItinerary.java
@@ -0,0 +1,21 @@
+package bst;
+
+import java.util.*;
+
+public class ReconstructItinerary {
+    Map<String, PriorityQueue<String>> targets = new HashMap<>();
+    List<String> route = new LinkedList();
+
+    public List<String> findItinerary(String[][] tickets) {
+        for (String[] ticket : tickets)
+            targets.computeIfAbsent(ticket[0], k -> new PriorityQueue()).add(ticket[1]);
+        visit("JFK");
+        return route;
+    }
+
+    void visit(String airport) {
+        while(targets.containsKey(airport) && !targets.get(airport).isEmpty())
+            visit(targets.get(airport).poll());
+        route.add(0, airport);
+    }
+}
