992576e16227db3f1b37289e93b8cc33ac07287b
touchty <touchxyz@163.com>
2018-06-04 16:05:48 +0800

    idea

---
+++ src/deepFirst/FlattenBinaryTreetoLinkedList.java
@@ -0,0 +1,52 @@
+package deepFirst;
+
+/*
+Given a binary tree, flatten it to a linked list in-place.
+
+For example, given the following tree:
+
+    1
+   / \
+  2   5
+ / \   \
+3   4   6
+The flattened tree should look like:
+
+1
+ \
+  2
+   \
+    3
+     \
+      4
+       \
+        5
+         \
+          6
+*/
+public class FlattenBinaryTreetoLinkedList {
+    public void flatten(TreeNode root) {
+        TreeNode cur = root, pre = root;
+        while (cur != null) {
+            if (cur.left != null) {
+                pre = cur.left;
+                while (pre.right != null && pre.right != cur) {
+                    pre = pre.right;
+                }
+                if (pre.right == null) {
+                    pre.right = cur;
+                    cur = cur.left;
+                } else {
+                    // instead of cut the temp link in Morris traversal, we link it to right subtree
+                    TreeNode right = cur.right;
+                    cur.right = cur.left;
+                    cur.left = null;
+                    pre.right = right;
+                    cur = right;
+                }
+            } else {
+                cur = cur.right;
+            }
+        }
+    }
+}
---
+++ src/deepFirst/LetterCombinationsofaPhoneNumber.java
@@ -0,0 +1,30 @@
+package deepFirst;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LetterCombinationsofaPhoneNumber {
+    public List<String> letterCombinations(String digits) {
+        if (digits.length() == 0)
+            return new ArrayList<String>();
+        String[] s = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        List<String> res = new ArrayList<>();
+        helper(s, res, new StringBuilder(), digits);
+        return res;
+    }
+
+    private void helper(String[] s, List<String> res, StringBuilder sb, String digits){
+        if (sb.length() == digits.length()){
+            String str = new String(sb);
+            res.add(str);
+            return;
+        }
+
+        char[] ch = s[digits.charAt(sb.length()) - '0'].toCharArray();
+        for (int i = 0; i < ch.length; i++){
+            sb.append(ch[i]);
+            helper(s, res, sb, digits);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}
