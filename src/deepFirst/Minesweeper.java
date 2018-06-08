326534fe0bbd7aa101fc8e50084fb6af979b93b8
touchty <touchxyz@163.com>
2018-06-08 23:03:23 +0800

    idea

---
+++ src/deepFirst/Minesweeper.java
@@ -0,0 +1,73 @@
+package deepFirst;
+
+public class Minesweeper {
+    public char[][] updateBoard(char[][] board, int[] click) {
+        int m = board.length, n = board[0].length;
+        int row = click[0], col = click[1];
+
+        if (board[row][col] == 'M') { // Mine
+            board[row][col] = 'X';
+        }
+        else { // Empty
+            // Get number of mines first.
+            int count = 0;
+            for (int i = -1; i < 2; i++) {
+                for (int j = -1; j < 2; j++) {
+                    if (i == 0 && j == 0) continue;
+                    int r = row + i, c = col + j;
+                    if (r < 0 || r >= m || c < 0 || c < 0 || c >= n) continue;
+                    if (board[r][c] == 'M' || board[r][c] == 'X') count++;
+                }
+            }
+
+            if (count > 0) { // If it is not a 'B', stop further DFS.
+                board[row][col] = (char)(count + '0');
+            }
+            else { // Continue DFS to adjacent cells.
+                board[row][col] = 'B';
+                for (int i = -1; i < 2; i++) {
+                    for (int j = -1; j < 2; j++) {
+                        if (i == 0 && j == 0) continue;
+                        int r = row + i, c = col + j;
+                        if (r < 0 || r >= m || c < 0 || c < 0 || c >= n) continue;
+                        if (board[r][c] == 'E') updateBoard(board, new int[] {r, c});
+                    }
+                }
+            }
+        }
+
+        return board;
+    }
+
+    // other dfs
+    /*
+    static final int[][] dirs = new int[][] {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
+
+    public char[][] updateBoard(char[][] board, int[] click) {
+        int x = click[0], y = click[1];
+        if (board[x][y] == 'M') board[x][y] = 'X';
+        else dfs(board, x, y);
+        return board;
+    }
+
+    private void dfs(char[][] board, int i, int j) {
+        if (board[i][j] != 'E') return;
+        int count = 0, n = board.length, m = board[0].length;
+        for (int[] dir : dirs) {
+            int x = i + dir[0], y = j + dir[1];
+            if (x >= 0 && y >= 0 && x < n && y < m && board[x][y] == 'M') count++;
+        }
+        if (count > 0) {
+            board[i][j] = (char)('0' + count);
+            return;
+        }
+
+        board[i][j] = 'B';
+        for (int[] dir : dirs) {
+            int x = i + dir[0], y = j + dir[1];
+            if (x >= 0 && y >= 0 && x < n && y < m && board[x][y] == 'E')
+                dfs(board, x, y);
+        }
+    }
+    */
+}
