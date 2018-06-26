53456575c5d2c9b6f34f95d7289ad2972a94c48a
touchty <touchxyz@163.com>
2018-06-26 16:32:21 +0800

    idea

---
+++ src/array/Subsets.java
@@ -0,0 +1,29 @@
+package array;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+        for(int n : nums) {
+            int size = result.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> subset = new ArrayList<>(result.get(i));
+                subset.add(n);
+                result.add(subset);
+            }
+        }
+
+        /*for(int n : nums) {
+            for (List<Integer> list : result) {
+                System.out.println(list);
+                List<Integer> subset = new ArrayList<>(list);
+                subset.add(n);
+                result.add(subset);  // modify the result List - throw exception
+            }
+        }*/
+        return result;
+    }
+}
