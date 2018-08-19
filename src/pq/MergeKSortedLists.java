358ab2d8be005c38d39de707c08d710de6a822d6
touchty <touchxyz@163.com>
2018-08-19 10:11:57 +0800

    restart training

---
+++ src/pq/MergeKSortedLists.java
@@ -0,0 +1,37 @@
+package pq;
+
+import java.util.PriorityQueue;
+
+/**
+ *
+ */
+public class MergeKSortedLists {
+    /**
+     *
+     * @param lists list of ListNode
+     * @return sorted ListNode
+     */
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        PriorityQueue<ListNode> queue = new PriorityQueue<>(lists.length,
+                (o1, o2) -> (o1.val - o2.val));
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        for (ListNode node : lists) {
+            if (node != null) {
+                queue.add(node);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            tail.next = queue.poll();
+            tail = tail.next;
+
+            if (tail.next != null) {
+                queue.add(tail.next);
+            }
+        }
+
+        return dummy.next;
+    }
+}
