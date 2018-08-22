4ee31164ecb5b847d589720bae89cabbfb1b7f90
Jeff <touchxyz@163.com>
2018-08-22 21:17:50 +0800

    training

---
+++ src/dp/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,23 @@
+package dp;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        int head = 0;
+        int end  = 0;
+        int counter = 0;
+        int[] map = new int[128];
+        int d = 0;
+
+        while (end < s.length()){
+            if (map[s.charAt(end++)]++ > 0) counter++;
+
+            while (counter > 0){
+                if (map[s.charAt(head++)]-- > 1) counter--;
+            }
+
+            d = Math.max(d, end - head);
+        }
+
+        return d;
+    }
+}
---
+++ src/dp/MaximalRectangle.java
@@ -0,0 +1,49 @@
+package dp;
+
+import java.util.Arrays;
+
+public class MaximalRectangle {
+    public int maximalRectangle(char[][] matrix) {
+        if(matrix == null || matrix[0].length == 0) return 0;
+
+        final int m = matrix.length;
+        final int n = matrix[0].length;
+
+        int[] left = new int[n], right = new int[n], height = new int[n];
+        Arrays.fill(right, n);
+        int maxA = 0;
+
+        for(int i=0; i<m; i++) {
+            int cur_left=0, cur_right=n;
+            for(int j=0; j<n; j++) { // compute height (can do this from either side)
+                if(matrix[i][j]=='1') height[j]++;
+                else height[j]=0;
+            }
+            for(int j=0; j<n; j++) { // compute left (from left to right)
+                if(matrix[i][j]=='1') left[j]=Math.max(left[j],cur_left);
+                else {left[j]=0; cur_left=j+1;}
+            }
+            // compute right (from right to left)
+            for(int j=n-1; j>=0; j--) {
+                if(matrix[i][j]=='1') right[j]=Math.min(right[j],cur_right);
+                else {right[j]=n; cur_right=j;}
+            }
+            // compute the area of rectangle (can do this from either side)
+            for(int j=0; j<n; j++)
+                maxA = Math.max(maxA,(right[j]-left[j])*height[j]);
+        }
+        return maxA;
+    }
+
+    public static void main(String[] args) {
+        char[][] matrix = {
+                          {'1','0','1','0','0'},
+                          {'1','0','1','1','1'},
+                          {'1','1','1','1','1'},
+                          {'1','0','0','1','0'}
+                        };
+        MaximalRectangle maximalRectangle = new MaximalRectangle();
+
+        System.out.println(maximalRectangle.maximalRectangle(matrix));
+    }
+}
