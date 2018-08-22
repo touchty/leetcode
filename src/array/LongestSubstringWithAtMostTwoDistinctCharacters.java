044f9ceb2f10e5b23b6ec77515d38b9b810eb4b8
touchty <touchxyz@163.com>
2018-08-22 15:18:54 +0800

    restart training

---
+++ src/array/LongestSubstringWithAtMostTwoDistinctCharacters.java
@@ -0,0 +1,26 @@
+package array;
+
+import org.junit.Assert;
+
+public class LongestSubstringWithAtMostTwoDistinctCharacters {
+    public static int lengthOfLongestSubstringTwoDistinct(String s) {
+        int[] map = new int[128];
+        int counter=0, begin=0, end=0, d=0;
+        while(end<s.length()){
+            if(map[s.charAt(end++)]++ == 0) counter++;
+            while(counter>2) if(map[s.charAt(begin++)]-- == 1) counter--;
+            d=Math.max(d, end-begin);
+        }
+        return d;
+    }
+
+    public static void main(String[] args) {
+        String str = "abcbbbbcccbdddadacb";
+
+        int result = LongestSubstringWithAtMostTwoDistinctCharacters.lengthOfLongestSubstringTwoDistinct(str);
+
+        int expected = 10;
+
+        Assert.assertEquals(result, expected);
+    }
+}
---
+++ src/array/MinimumWindowsSubstring.java
@@ -0,0 +1,35 @@
+package array;
+
+public class MinimumWindowsSubstring {
+    public String minWindow(String s, String t) {
+        int[] map = new int[128];
+
+        for (char c : t.toCharArray()) map[c]++;
+
+        int count = t.length(), begin = 0, end = 0, d = Integer.MAX_VALUE, head = 0;
+
+        while (end < s.length()) {
+            if (map[s.charAt(end++)]-- > 0) count--;  //in t
+            while (count == 0) {  // valid
+                if (end - begin < d) {
+                    d = end - begin;
+                    head = begin;
+                }
+                if (map[s.charAt(begin++)]++ == 0)  //make it invalid.
+                    count++;  // s.charAt(begin) is in target
+            }
+        }
+
+        return d == Integer.MAX_VALUE ? "" : s.substring(head, head+d);
+    }
+
+    public static void main(String[] args) {
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+
+        MinimumWindowsSubstring minimumWindowsSubstring = new MinimumWindowsSubstring();
+        String result = minimumWindowsSubstring.minWindow(s, t);
+        System.out.println(result);
+    }
+
+}
