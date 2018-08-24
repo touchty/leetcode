5ccb4ced5e99b2c605c3cba40db05d422bc7bd62
Jeff <touchxyz@163.com>
2018-08-24 16:08:37 +0800

    training!

---
+++ src/array/SingleNumber.java
@@ -0,0 +1,52 @@
+package array;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+ *
+ * Note:
+ *
+ * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ *
+ * Example 1:
+ *
+ * Input: [2,2,1]
+ * Output: 1
+ * Example 2:
+ *
+ * Input: [4,1,2,1,2]
+ * Output: 4
+ */
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums){
+            if (!map.containsKey(num))
+                map.put(num, 1);
+            else map.remove(num);
+        }
+
+        Set<Integer> keys = map.keySet();
+        if (keys.size() == 1) {
+            Integer[] arr = new Integer[1];
+            keys.toArray(arr);
+            return arr[0];
+        }
+        else return -1;
+    }
+
+    public int singleNumberOpt(int[] nums){
+        if (nums == null || nums.length == 0)
+            return -1;
+        int sum = 0;
+
+        for (int num : nums){
+            sum ^= num;
+        }
+        return sum;
+    }
+}
---
+++ src/treeMap/LongestConsecutiveSequence.java
@@ -0,0 +1,57 @@
+package treeMap;
+
+import org.junit.Assert;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class LongestConsecutiveSequence {
+    public int longestConsecutive(int[] nums) {
+        int longest = 0;
+        if (nums == null || nums.length ==0) {
+            return 0;
+        }
+
+        Arrays.sort(nums);
+        HashMap<Integer, Integer> consecutiveLength = new HashMap<>();
+
+        for (int num : nums){
+            if (!consecutiveLength.containsKey(num)){
+                int prev = consecutiveLength.getOrDefault(num - 1, 0) + 1;
+                longest = Math.max(longest, prev);
+                consecutiveLength.put(num, prev);
+            }
+        }
+        return longest;
+    }
+
+    public int longestConsecutiveV2(int[] nums) {
+        if (nums == null || nums.length ==0) {
+            return 0;
+        }
+
+        Arrays.sort(nums);
+
+        int max = 1;
+        int current = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1] + 1) {
+                current++;
+            }else if(nums[i] != nums[i - 1]){
+                current = 1;
+            }
+            max = Math.max(max, current);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{100, 4, 200, 1, 3, 2};
+
+        int longest = new LongestConsecutiveSequence().longestConsecutiveV2(nums);
+        int expected = 4;
+        System.out.println(longest);
+        Assert.assertEquals(longest, expected);
+        System.out.println("No Error.");
+    }
+}
