656da6e3210f10ea4b344908f31096b3b3583e1d
Jeff <touchxyz@163.com>
2018-08-26 10:43:25 +0800

    keep training!

--- src/stack/MinStack.java
+++ src/stack/MinStack.java
@@ -57,6 +57,44 @@ class MinStack {
         else
             return null;
     }
+    class MinStackOpt {
+        private Node head;
+
+        public void push(int x) {
+            if(head == null)
+                head = new Node(x, x);
+            else
+                head = new Node(x, Math.min(x, head.min), head);
+        }
+
+        public void pop() {
+            head = head.next;
+        }
+
+        public int top() {
+            return head.val;
+        }
+
+        public int getMin() {
+            return head.min;
+        }
+
+        private class Node {
+            int val;
+            int min;
+            Node next;
+
+            private Node(int val, int min) {
+                this(val, min, null);
+            }
+
+            private Node(int val, int min, Node next) {
+                this.val = val;
+                this.min = min;
+                this.next = next;
+            }
+        }
+    }
 
     public static void main(String[] args) {
         MinStack minStack = new MinStack();
