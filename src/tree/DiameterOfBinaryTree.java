08623e0464354a55285c62141ecc7595c2d1a895
Jeff <touchxyz@163.com>
2018-08-29 22:29:31 +0800

    543. Diameter of Binary Tree

--- src/tree/DiameterOfBinaryTree.java
+++ src/tree/DiameterOfBinaryTree.java
@@ -24,4 +24,31 @@ public class DiameterOfBinaryTree {
 
         return Math.max(left, right) + 1;  // one path, no fork
     }
+
+    public int diameterOfBinaryTreeOpt(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        int[] max = new int[1];
+        helper (root, max);
+        return max[0];
+
+    }
+
+    private int helper(TreeNode root, int[] max) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        int left = helper(root.left, max);
+        int right = helper(root.right, max);
+        if (left + right  > max[0]) {
+            max[0] = left + right ;
+        }
+        return Math.max(left, right) + 1;
+    }
 }
