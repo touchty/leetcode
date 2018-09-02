d380b7a724da36411620630db035fd222796ddbf
touchty <touchxyz@163.com>
2018-09-02 11:16:41 +0800

    Valid Sudoku

--- src/array/ValidSudoku.java
+++ src/array/ValidSudoku.java
@@ -19,6 +19,32 @@ public class ValidSudoku {
         return true;
     }
 
+    public boolean isValidSudokuOpt(char[][] board) {
+        for(int i = 0; i<9; i++){
+            HashSet<Character> rows = new HashSet<Character>();
+            HashSet<Character> columns = new HashSet<Character>();
+
+            for (int j = 0; j < 9;j++){
+                if(board[i][j]!='.' && !rows.add(board[i][j]))
+                    return false;
+                if(board[j][i]!='.' && !columns.add(board[j][i]))
+                    return false;
+            }
+        }
+
+        for(int i = 0; i < 7;i += 3) {
+            for(int j = 0;j < 7; j += 3){
+                HashSet<Character> cube = new HashSet<Character>();
+                for(int k = 0;k < 9;k++){
+                    if(board[i + k/3][j + k % 3]!='.' && !cube.add(board[i + k/3][j + k % 3]))
+                        return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
         char[][] board = {{'5','3','.','.','7','.','.','.','.'},{'6','.','.','1','9','5','.','.','.'},{'.','9','8','.','.','.','.','6','.'},{'8','.','.','.','6','.','.','.','3'},{'4','.','.','8','.','3','.','.','1'},{'7','.','.','.','2','.','.','.','6'},{'.','6','.','.','.','.','2','8','.'},{'.','.','.','4','1','9','.','.','5'},{'.','.','.','.','8','.','.','7','9'}};
 
