2e84f347cb89405222f303fd549f0a0c15600b0e
Jeff <touchxyz@163.com>
2018-09-13 08:54:29 +0800

    797. All Paths From Source to Target

---
+++ src/graph/AllPathsFromSourceToTarget.java
@@ -0,0 +1,64 @@
+package graph;
+
+import java.util.*;
+
+public class AllPathsFromSourceToTarget {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        Map<Integer, List<List<Integer>>> map = new HashMap<>();
+
+        return dfsSearch(graph, 0, map);
+    }
+
+    private List<List<Integer>> dfsSearch(int[][] graph, int node, Map<Integer, List<List<Integer>>> map) {
+        if (map.containsKey(node)) {
+            return map.get(node);
+        }
+
+        List<List<Integer>> res = new ArrayList<>();
+        if (node == graph.length - 1) {
+            List<Integer> path = new LinkedList<>();
+            path.add(node);
+            res.add(path);
+        } else {
+            for (int nextNode : graph[node]) {
+                List<List<Integer>> subPaths = dfsSearch(graph, nextNode, map);
+                for (List<Integer> path : subPaths) {
+                    LinkedList<Integer> newPath = new LinkedList<>(path);
+                    newPath.addFirst(node);
+                    res.add(newPath);
+                }
+            }
+        }
+
+        map.put(node, res);
+        return res;
+    }
+
+    // Opt
+    class Solution {
+        public List<List<Integer>> allPathsSourceTarget(int[][] graph)
+        {
+            List<List<Integer>>path = new ArrayList<>();
+            List<Integer>list = new ArrayList<>();
+            list.add(0);
+            path_utils(path,list,graph,0,graph.length-1);
+
+            return path;
+        }
+
+        public void path_utils(List<List<Integer>>path_list,List<Integer>path,int[][] graph,int s,int e){
+            if(s == e){
+                path_list.add(new ArrayList<Integer>(path));
+                return;
+            }
+
+            for(int i:graph[s]){
+                path.add(i);
+                path_utils(path_list,path,graph,i,e);
+                path.remove(path.size()-1);
+            }
+
+        }
+
+    }
+}
---
+++ src/linkedList/LinkedListDemo.java
@@ -0,0 +1,29 @@
+package linkedList;
+
+import java.awt.print.PrinterGraphics;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        for (int i = 0; i < 10; i++){
+            list.add(i);
+        }
+
+        list = new LinkedList<>();
+        for (int i = 0; i < 10; i++){
+            list.addFirst(i);
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() > 0){
+            System.out.println(queue.remove());
+        }
+    }
+}
