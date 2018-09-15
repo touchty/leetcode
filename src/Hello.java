ad105d627e3ae896f983941d1b69bc16771c3bd1
Jeff <touchxyz@163.com>
2018-09-15 10:38:10 +0800

    rearrange project structure

--- src/Hello.java
+++
@@ -1,7 +0,0 @@
-import java.util.Arrays;
-
-public class Hello {
-    public static void main(String[] args) {
-        int pos = Arrays.binarySearch(new int[]{1,2,3,4,5,6,7,8}, 7);
-    }
-}
---
+++ src/main/java/Hello.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.Complex;
+import edu.princeton.cs.algs4.FFT;
+public class Hello {
+    public static void main(String[] args) {
+        int pos = Arrays.binarySearch(new int[]{1,2,3,4,5,6,7,8}, 7);
+        Complex[] data = new Complex[2];
+        Complex i1 = new Complex(1,2);
+        Complex i2 = new Complex(3,4);
+        data[0] = i1;
+        data[1] = i2;
+        Complex[] fft = FFT.fft(data);
+        System.out.println("No smoke!");
+        for (Complex c : data)
+            System.out.println(c);
+
+        for (Complex c : fft)
+            System.out.println(c);
+    }
+}
