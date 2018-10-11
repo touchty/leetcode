8e2a3556fe8ce79028efafc6999974924fb634a6
Tao Yao <touchxyz@163.com>
2018-10-11 19:34:23 +0800

    165	Compare Version Numbers

---
+++ src/main/java/array/CompareVersionNumbers.java
@@ -0,0 +1,18 @@
+package array;
+
+public class CompareVersionNumbers {
+    public int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+
+        int max = v1.length < v2.length? v2.length: v1.length;
+        for (int i = 0; i < max; i++) {
+            int int1 = (i < v1.length)? Integer.parseInt(v1[i]): 0;
+            int int2 = (i < v2.length)? Integer.parseInt(v2[i]): 0;
+            if (int1 < int2) { return -1; }
+            if (int2 < int1) { return 1; }
+        }
+
+        return 0;
+    }
+}
