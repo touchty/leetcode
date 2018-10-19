f5e70554707ddf3b4d1dcef0675e4b83d923d620
Tao Yao <touchxyz@163.com>
2018-10-19 10:15:13 +0800

    67. Add Binary

---
+++ src/main/java/array/AddBinary.java
@@ -0,0 +1,60 @@
+package array;
+
+/**
+ * Given two binary strings, return their sum (also a binary string).
+ *
+ * The input strings are both non-empty and contains only characters 1 or 0.
+ *
+ * Example 1:
+ *
+ * Input: a = "11", b = "1"
+ * Output: "100"
+ * Example 2:
+ *
+ * Input: a = "1010", b = "1011"
+ * Output: "10101"
+ */
+public class AddBinary {
+    public String addBinary(String a, String b) {
+        int mxLen = a.length() > b.length() ? a.length() : b.length();
+        int[] res = new int[mxLen + 1];
+        int aPtr = a.length() - 1;
+        int bPtr = b.length() - 1;
+
+        int i = mxLen;
+        int sum = 0;
+        int carry = 0;
+        while (aPtr >= 0 && bPtr >= 0) {
+            sum = (a.charAt(aPtr--) - '0' +b.charAt(bPtr--) - '0') +  carry;
+            res[i] = sum % 2;
+            carry = sum / 2;
+            i--;
+        }
+
+        while (aPtr >= 0) {
+            sum = (a.charAt(aPtr--) - '0') +  carry;
+            res[i] = sum % 2;
+            carry = sum / 2;
+            i--;
+        }
+
+        while (bPtr >= 0) {
+            sum = (b.charAt(bPtr--) - '0') +  carry;
+            res[i] = sum % 2;
+            carry = sum / 2;
+            i--;
+        }
+
+        res[0] = carry;
+
+        StringBuilder builder = new StringBuilder();
+        if (res[0] == 1)
+            for (int e : res)
+                builder.append(e);
+        else
+            for (int j = 1; j <= mxLen; j++)
+                builder.append(res[j]);
+
+        return builder.toString();
+    }
+}
