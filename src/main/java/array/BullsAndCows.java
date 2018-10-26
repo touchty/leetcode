116f93e088398205a818c0ff22f04ce76925efc1
Tao Yao <touchxyz@163.com>
2018-10-26 09:34:28 +0800

    303. Range Sum Query - Immutable

---
+++ src/main/java/array/BullsAndCows.java
@@ -0,0 +1,23 @@
+package array;
+
+public class BullsAndCows {
+    public static String getHint(String secret, String guess) {
+        int cows = 0;
+        int bulls = 0;
+        int[] counts = new int[10];
+        for (int i= 0; i < secret.length(); i++) {
+            if (secret.charAt(i) == guess.charAt(i)) bulls++;
+            else{
+                if (counts[secret.charAt(i) - '0']++ < 0) cows++;
+                if (counts[guess.charAt(i) - '0']-- > 0) cows++;
+            }
+        }
+        return bulls + "A" + cows + "B";
+    }
+
+
+    public static void main(String[] args) {
+        String secret = "1123", guess = "0111";
+        System.out.println(getHint(secret, guess));
+    }
+}
---
+++ src/main/java/array/RangeSumQueryImmutable.java
@@ -0,0 +1,37 @@
+package array;
+
+/**
+ * Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+ *
+ * Example:
+ * Given nums = [-2, 0, 3, -5, 2, -1]
+ *
+ * sumRange(0, 2) -> 1
+ * sumRange(2, 5) -> -1
+ * sumRange(0, 5) -> -3
+ */
+public class RangeSumQueryImmutable {
+    int[] nums;
+    int[] sums;
+    public RangeSumQueryImmutable(int[] nums) {
+        this.nums = nums;
+        sums = new int[nums.length];
+        sums[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sums[i] = sums[i -1] + nums[i];
+        }
+    }
+
+    public int sumRange(int i, int j) {
+        if (i == 0)
+            return sums[j];
+        else
+            return sums[j] - sums[i - 1];
+    }
+
+    public static void main(String[] args) {
+       int[] nums = {-2, 0, 3, -5, 2, -1};
+        RangeSumQueryImmutable rangeSumQueryImmutable = new RangeSumQueryImmutable(nums);
+        System.out.println(rangeSumQueryImmutable.sumRange(0, 5));
+    }
+}
