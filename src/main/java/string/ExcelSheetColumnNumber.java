528a909ccc61575c1437b34bac42fb0b7a0cff46
Tao Yao <touchxyz@163.com>
2018-10-26 10:46:24 +0800

    171. Excel Sheet Column Number

---
+++ src/main/java/string/ExcelSheetColumnNumber.java
@@ -0,0 +1,45 @@
+package string;
+
+/**
+ * Given a column title as appear in an Excel sheet, return its corresponding column number.
+ *
+ * For example:
+ *
+ *     A -> 1
+ *     B -> 2
+ *     C -> 3
+ *     ...
+ *     Z -> 26
+ *     AA -> 27
+ *     AB -> 28
+ *     ...
+ * Example 1:
+ *
+ * Input: "A"
+ * Output: 1
+ * Example 2:
+ *
+ * Input: "AB"
+ * Output: 28
+ * Example 3:
+ *
+ * Input: "ZY"
+ * Output: 701
+ */
+public class ExcelSheetColumnNumber {
+    final int ALPHABET = 26;
+
+    public int titleToNumber(String s) {
+        int Len = s.length();
+        int weight = 1;
+        int count = 0;
+
+        for (int i = Len -1; i >= 0; i--) {
+            int val = s.charAt(i) - 'A' + 1;
+            count += val * weight;
+            weight *= ALPHABET;
+        }
+
+        return count;
+    }
+}
