734a3ed230112ebd65c5988a9fef4f7f8b307f51
Tao Yao <touchxyz@163.com>
2018-11-17 21:25:08 +0800

    257. Binary Tree Paths

---
+++ src/main/java/string/BinaryTreePaths.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class BinaryTreePaths {
+    public List<String> binaryTreePaths(TreeNode root) {
+            List<String> paths = new ArrayList<>();
+            if (root == null)
+                return paths;
+            
+            dfs(root, "", paths);
+            return paths;
+        }
+        
+        void dfs(TreeNode root, String path, List<String> paths) {
+            if (root == null) {
+                return;
+            }
+            path += ( root.val + "->");
+            
+            if (root.left == null && root.right == null) {
+                paths.add(path.substring(0, path.length() - 2));
+                return;
+            }
+            else {
+                dfs(root.left, path, paths);
+                dfs(root.right, path, paths);
+            }
+        }
+}
