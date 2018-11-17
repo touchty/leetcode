492b52d99f302d2a0542ca9c04fe86e22f89ab6c
Tao Yao <touchxyz@163.com>
2018-11-17 20:46:56 +0800

    258. Add Digits

---
+++ src/main/java/algorithms/AddDigits.java
@@ -0,0 +1,29 @@
+class AddDigits{
+    public int addDigits(int num) {
+        while (num >= 10) {
+            int temp = 0;
+            while (num >= 10) {
+                temp += num % 10;
+                num /= 10;
+            }
+            temp += num;
+            num = temp;
+        }
+        return num;
+    }
+
+    public int addDigitsOpt(int num) {
+        
+        if(num<10){
+            return num;
+        }
+        
+        int sum=0;
+        while(num>=10){
+            sum+=num%10;
+            num/=10;
+        }
+        sum+=num;
+        return addDigitsOpt(sum);
+    }
+}
