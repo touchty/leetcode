3c305ffa57ba829ebb3fa41934373810e8cfd3bb
Tao Yao <touchxyz@163.com>
2018-12-02 14:59:36 +0800

    443. String Compression

---
+++ src/main/java/string/StringCompression.java
@@ -0,0 +1,39 @@
+package string;
+
+/**
+ * Given an array of characters, compress it in-place.
+ * The length after compression must always be smaller than or equal to the original array.
+ * Every element of the array should be a character (not int) of length 1.
+ * After you are done modifying the input array in-place, return the new length of the array.
+ *
+ * Example 1:
+ *
+ * Input:
+ * ["a","a","b","b","c","c","c"]
+ *
+ * Output:
+ * Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+ *
+ * Explanation:
+ * "aa" is replaced by "a2". "bb" is replaced by "b2". "ccc" is replaced by "c3".
+ */
+public class StringCompression {
+    public int compress(char[] chars) {
+        int indexAns = 0, index = 0;
+        while (index < chars.length) {
+            char currChar = chars[index];
+            int count = 0;
+            while (index < chars.length && chars[index] == currChar) {
+                count++;
+                index++;
+            }
+            chars[indexAns++] = currChar;
+            if (count != 1) {
+                for (char c : Integer.toString(count).toCharArray()) {
+                    chars[indexAns++] = c;
+                }
+            }
+        }
+        return indexAns;
+    }
+}
