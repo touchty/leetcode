8d2a70ed62355204ddef558275a2ad5720d8bed4
touchty <touchxyz@163.com>
2018-12-06 23:58:03 +0800

    create LinkedList from int[]

---
+++ src/main/java/linkedList/LinkedListHelper.java
@@ -0,0 +1,25 @@
+package linkedList;
+
+public class LinkedListHelper {
+    public static ListNode create(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        int LEN = nums.length;
+        ListNode[] nodes = new ListNode[LEN];
+        for (int i = 0; i < LEN; i++) {
+            nodes[i] = new ListNode(nums[i]);
+        }
+        for (int i = 0; i < LEN - 1; i++) {
+            nodes[i].next = nodes[i + 1];
+        }
+
+        return nodes[0];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1};
+        ListNode root = create(nums);
+        System.out.println(root.val);
+    }
+}
