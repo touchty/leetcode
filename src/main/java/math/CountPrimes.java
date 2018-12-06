95e004c7d420f0c2aa4087de8fcbb1aad17b0180
touchty <touchxyz@163.com>
2018-12-06 20:45:47 +0800

    204. Count Primes

---
+++ src/main/java/math/CountPrimes.java
@@ -0,0 +1,25 @@
+package math;
+
+/**
+ * Count the number of prime numbers less than a non-negative number, n.
+ *
+ * Example:
+ * Input: 10
+ * Output: 4
+ * Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+ */
+public class CountPrimes {
+    public int countPrimes(int n) {
+        boolean[] isPrime = new boolean[n];
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (!isPrime[i]) {
+                count++;
+            }
+            for (int j = 2; i * j < n; j++) {
+                isPrime[i * j] = true;
+            }
+        }
+        return count;
+    }
+}
