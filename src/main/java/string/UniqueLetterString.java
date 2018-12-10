807bafbd20cc1a07b1da9d589e5518f0cf6d369f
Tao Yao <touchxyz@163.com>
2018-12-10 15:28:14 +0800

    828. Unique Letter String

---
+++ src/main/java/string/UniqueLetterString.java
@@ -0,0 +1,98 @@
+package string;
+
+import org.junit.Assert;
+
+import java.util.Arrays;
+
+/**
+ * A character is unique in string S if it occurs exactly once in it.
+ *
+ * For example, in string S = "LETTER", the only unique characters are "L" and "R".
+ *
+ * Let's define UNIQ(S) as the number of unique characters in string S.
+ *
+ * For example, UNIQ("LETTER") =  2.
+ *
+ * Given a string S with only uppercases, calculate the sum of UNIQ(substring) over all non-empty substrings of S.
+ *
+ * If there are two or more equal substrings at different positions in S, we consider them different.
+ *
+ * Since the answer can be very large, return the answer modulo 10 ^ 9 + 7.
+ */
+
+/*
+Intuition:
+
+Let's think about how a character can be found as a unique character.
+
+Think about string "XAXAXXAX" and focus on making the second "A" a unique character.
+We can take "XA(XAXX)AX" and between "()" is our substring.
+We can see here, to make the second "A" counted as a uniq character, we need to:
+
+insert "(" somewhere between the first and second A
+insert ")" somewhere between the second and third A
+For step 1 we have "A(XA" and "AX(A", 2 possibility.
+For step 2 we have "A)XXA", "AX)XA" and "AXX)A", 3 possibilities.
+
+So there are in total 2 * 3 = 6 ways to make the second A a unique character in a substring.
+In other words, there are only 6 substring, in which this A contribute 1 point as unique string.
+
+Instead of counting all unique characters and struggling with all possible substrings,
+we can count for every char in S, how many ways to be found as a unique char.
+We count and sum, and it will be out answer.
+
+Explanation:
+
+index[26][2] record last two occurrence index for every upper characters.
+Initialise all values in index to -1.
+Loop on string S, for every character c, update its last two occurrence index to index[c].
+Count when loop. For example, if "A" appears twice at index 3, 6, 9 seperately, we need to count:
+For the first "A": (6-3) * (3-(-1))"
+For the second "A": (9-6) * (6-3)"
+For the third "A": (N-9) * (9-6)"
+Complexity:
+One pass, time complexity O(N).
+Space complexity O(1).
+*/
+
+public class UniqueLetterString {
+    public static int uniqueLetterString(String S) {
+        int[][] index = new int[26][2];
+        for (int i = 0; i < 26; ++i) Arrays.fill(index[i], -1);
+        int res = 0, N = S.length(), mod = (int)Math.pow(10, 9) + 7;
+        for (int i = 0; i < N; ++i) {
+            int c = S.charAt(i) - 'A';
+            res = (res + (i - index[c][1]) * (index[c][1] - index[c][0]) % mod) % mod;
+            index[c] = new int[] {index[c][1], i};
+        }
+        for (int c = 0; c < 26; ++c)
+            res = (res + (N - index[c][1]) * (index[c][1] - index[c][0]) % mod) % mod;
+        return res;
+    }
+
+    public static int uniqueLetterStringOpt(String S) {
+        int res = 0;
+        int mod = (int)Math.pow(10, 9) + 7;
+        int[][] index = new int[26][2];
+        for (int i = 0; i < 26; ++i) Arrays.fill(index[i], -1);
+        for (int i = 0; i < S.length(); i++) {
+            int c = S.charAt(i) - 'A';
+            res = (res + (i - index[c][1]) * (index[c][1] - index[c][0]) % mod) % mod;
+            index[c] = new int[] {index[c][1], i};
+        }
+        for (int c = 0; c < 26; ++c)
+            res = (res + (S.length() - index[c][1]) * (index[c][1] - index[c][0]) % mod) % mod;
+        return res;
+    }
+    public static void main(String[] args) {
+        String[] testCases = new String[]{"ABC", "ABA"};
+        int cases = testCases.length;
+        int[] expected = {10, 8};
+        int[] res = new int[cases];
+        for (int i = 0; i < testCases.length; i++) {
+            res[i] = uniqueLetterStringOpt(testCases[i]);
+        }
+
+        Assert.assertArrayEquals(expected, res);
+    }
+}
