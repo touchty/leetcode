03fbd7aff3d5d734d21984b0ac65a128ab04f511
touchty <touchxyz@163.com>
2019-01-02 23:56:05 +0800

    revise

--- src/main/java/dp/CoinChange.java
+++ src/main/java/dp/CoinChange.java
@@ -1,8 +1,21 @@
 package dp;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * 322. Coin Change
+ * You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+ *
+ * Example 1:
+ *
+ * Input: coins = [1, 2, 5], amount = 11
+ * Output: 3
+ * Explanation: 11 = 5 + 5 + 1
+ * Example 2:
+ *
+ * Input: coins = [2], amount = 3
+ * Output: -1
+ */
 public class CoinChange {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
