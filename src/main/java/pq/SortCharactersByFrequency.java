e9b7332f58fb4ccfe4dddc131ab8d663f8a8126e
touchty <touchxyz@163.com>
2019-01-13 11:03:34 +0800

    451. Sort Characters By Frequency

---
+++ src/main/java/pq/SortCharactersByFrequency.java
@@ -0,0 +1,39 @@
+package pq;
+
+import java.util.*;
+
+public class SortCharactersByFrequency {
+    public static String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        List<Map.Entry<Character, Integer>> list = new ArrayList<>();
+        pq.addAll(map.entrySet());
+        list.addAll(map.entrySet());
+        list.sort((a, b) -> b.getValue() - a.getValue());
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        while (!pq.isEmpty()) {
+            Map.Entry e = pq.poll();
+            for (int i = 0; i < (int)e.getValue(); i++)
+                sb.append(e.getKey());
+        }
+        for (Map.Entry<Character, Integer> entry : list) {
+            for (int i = 0; i < (int) entry.getValue(); i++)
+                sb2.append(entry.getKey());
+        }
+        return sb.toString();
+        // return sb2.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "tree";
+        String res = SortCharactersByFrequency.frequencySort(str);
+        System.out.println(res);
+    }
+
+}
