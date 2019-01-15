3b97af588cf11ff9fffa3195fb76819446b8e68a
Tao Yao <touchxyz@163.com>
2019-01-15 09:47:09 +0800

    151. Reverse Words in a String

--- src/main/java/string/ReverseWordsinaString.java
+++ src/main/java/string/ReverseWordsinaString.java
@@ -3,18 +3,18 @@ package string;
 /**
  * 151. Reverse Words in a String
  * Medium
- *
+ * <p>
  * 423
- *
+ * <p>
  * 1998
- *
+ * <p>
  * Favorite
- *
+ * <p>
  * Share
  * Given an input string, reverse the string word by word.
- *
+ * <p>
  * Example:
- *
+ * <p>
  * Input: "the sky is blue",
  * Output: "blue is sky the".
  */
@@ -22,8 +22,8 @@ public class ReverseWordsinaString {
     public static String reverseWords(String s) {
         String[] stringArr = s.split(" ");
         StringBuilder builder = new StringBuilder();
-        for (int i = stringArr.length - 1; i  >= 0; i--) {
-            if(!stringArr[i].isEmpty())
+        for (int i = stringArr.length - 1; i >= 0; i--) {
+            if (!stringArr[i].isEmpty())
                 builder.append(stringArr[i]).append(" ");
         }
         if (builder.length() > 0)
