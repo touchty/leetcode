7bf0290d0ac4dab3e5fb6009048ea363be5ad11b
touchty <touchxyz@163.com>
2019-01-18 16:51:09 +0800

    648. Replace Words

--- src/main/java/trie/ReplaceWords.java
+++ src/main/java/trie/ReplaceWords.java
@@ -51,31 +51,61 @@ public class ReplaceWords {
     class SolutionOpt {
         public String replaceWords(List<String> dict, String sentence) {
             String[] tokens = sentence.split(" ");
-            Trie trie = new Trie();
-            for (String str : dict)
-                trie.insert(str);
+            TrieNode trie = buildTrie(dict);
             return replaceWords(tokens, trie);
         }
-        String replaceWords(String[] tokens, Trie trie) {
+
+        private String replaceWords(String[] tokens, TrieNode root) {
             StringBuilder stringBuilder = new StringBuilder();
             for (String token : tokens) {
-                Trie.TrieNode temp = trie.root;
-                for (int i = 0; i < token.length(); i++) {
-                    if (temp.nodes[token.charAt(i) - 'a'] != null) {
-                        temp = temp.nodes[token.charAt(i) - 'a'];
-                        if (temp.isLeaf) {
-                            stringBuilder.append(token.substring(0, i + 1)).append(" ");
-                            break;
-                        }
+                stringBuilder.append(getShortestReplacement(token, root));
+                stringBuilder.append(" ");
+            }
+            return stringBuilder.substring(0, stringBuilder.length()-1);
+        }
 
+        private String getShortestReplacement(String token, final TrieNode root) {
+            TrieNode temp = root;
+            StringBuilder stringBuilder = new StringBuilder();
+            for (char c : token.toCharArray()) {
+                stringBuilder.append(c);
+                if (temp.children[c - 'a'] != null) {
+                    if (temp.children[c - 'a'].isWord) {
+                        return stringBuilder.toString();
                     }
-                    else {
-                        stringBuilder.append(token).append(" ");
-                        break;
-                    }
+                    temp = temp.children[c - 'a'];
+                } else {
+                    return token;
                 }
             }
-            return stringBuilder.toString();
+            return token;
+        }
+
+        private TrieNode buildTrie(List<String> dict) {
+            TrieNode root = new TrieNode(' ');
+            for (String word : dict) {
+                TrieNode temp = root;
+                for (char c : word.toCharArray()) {
+                    if (temp.children[c - 'a'] == null) {
+                        temp.children[c - 'a'] = new TrieNode(c);
+                    }
+                    temp = temp.children[c - 'a'];
+                }
+                temp.isWord = true;
+            }
+            return root;
+        }
+
+        public class TrieNode {
+            char val;
+            TrieNode[] children;
+            boolean isWord;
+
+            public TrieNode(char val) {
+                this.val = val;
+                this.children = new TrieNode[26];
+                this.isWord = false;
+            }
         }
     }
 
