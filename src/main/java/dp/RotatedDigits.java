efbc09ab12ad7f618d92d39130b44f7274adfcb7
touchty <touchxyz@163.com>
2019-01-18 16:48:02 +0800

    788. Rotated Digits

---
+++ src/main/java/dp/RotatedDigits.java
@@ -0,0 +1,57 @@
+package dp;
+
+/**
+ * 788. Rotated Digits
+ * Easy
+ *
+ * 152
+ *
+ * 519
+ *
+ * Favorite
+ *
+ * Share
+ * X is a good number if after rotating each digit individually by 180 degrees, we get a valid number that is different from X.  Each digit must be rotated - we cannot choose to leave it alone.
+ *
+ * A number is valid if each digit remains a digit after rotation. 0, 1, and 8 rotate to themselves; 2 and 5 rotate to each other; 6 and 9 rotate to each other, and the rest of the numbers do not rotate to any other number and become invalid.
+ *
+ * Now given a positive number N, how many numbers X from 1 to N are good?
+ *
+ * Example:
+ * Input: 10
+ * Output: 4
+ * Explanation:
+ * There are four good numbers in the range [1, 10] : 2, 5, 6, 9.
+ * Note that 1 and 10 are not good numbers, since they remain unchanged after rotating.
+ */
+
+/*
+dp Solution:
+Using a int[] for dp.
+dp[i] = 0, invalid number
+dp[i] = 1, valid and same number
+dp[i] = 2, valid and different number
+*/
+public class RotatedDigits {
+    public int rotatedDigits(int N) {
+        int[] dp = new int[N + 1];
+        int count = 0;
+        for(int i = 0; i <= N; i++){
+            if(i < 10){
+                if(i == 0 || i == 1 || i == 8) dp[i] = 1;
+                else if(i == 2 || i == 5 || i == 6 || i == 9){
+                    dp[i] = 2;
+                    count++;
+                }
+            } else {
+                int a = dp[i / 10], b = dp[i % 10];
+                if(a == 1 && b == 1) dp[i] = 1;
+                else if(a >= 1 && b >= 1){
+                    dp[i] = 2;
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
