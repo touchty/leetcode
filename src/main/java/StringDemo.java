34432f069fb9cbd8a5aa07b2c2fc5d8a472df3d3
touchty <touchxyz@163.com>
2019-01-18 17:07:45 +0800

    796. Rotate String

---
+++ src/main/java/StringDemo.java
@@ -0,0 +1,23 @@
+public class StringDemo {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "Hello";
+        String s3 = new StringBuilder("Hello").toString();
+        System.out.println("==");
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s3);
+        System.out.println(s2 == s3);
+        System.out.println("String equals()");
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
+        System.out.println(s2.equals(s3));
+        System.out.println("hashCode");
+        System.out.println(s1.hashCode());
+        System.out.println(s2.hashCode());
+        System.out.println(s3.hashCode());
+        System.out.println("System.identityHashCode");
+        System.out.println(System.identityHashCode(s1));
+        System.out.println(System.identityHashCode(s2));
+        System.out.println(System.identityHashCode(s3));
+    }
+}
