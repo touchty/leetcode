f5cf4e3089b62a8329569c64ac7f9548a648a7f2
Tao Yao <touchxyz@163.com>
2019-01-19 13:45:56 +0800

    301. Remove Invalid Parentheses

--- src/main/java/stack/RemoveInvalidParentheses.java
+++ src/main/java/stack/RemoveInvalidParentheses.java
@@ -41,11 +41,16 @@ Do not need preprocess.
 Runtime 3 ms.
 Explanation:
 We all know how to check a string of parentheses is valid using a stack. Or even simpler use a counter.
-The counter will increase when it is ‘(‘ and decrease when it is ‘)’. Whenever the counter is negative, we have more ‘)’ than ‘(‘ in the prefix.
+The counter will increase when it is ‘(‘ and decrease when it is ‘)’. Whenever the counter is negative, we have more ‘)’
+than ‘(‘ in the prefix.
 
-To make the prefix valid, we need to remove a ‘)’. The problem is: which one? The answer is any one in the prefix. However, if we remove any one, we will generate duplicate results, for example: s = ()), we can remove s[1] or s[2] but the result is the same (). Thus, we restrict ourself to remove the first ) in a series of concecutive )s.
+To make the prefix valid, we need to remove a ‘)’. The problem is: which one? The answer is any one in the prefix.
+However, if we remove any one, we will generate duplicate results, for example: s = ()), we can remove s[1] or s[2] but
+the result is the same (). Thus, we restrict ourselves to remove the first ) in a series of consecutive )s.
 
-After the removal, the prefix is then valid. We then call the function recursively to solve the rest of the string. However, we need to keep another information: the last removal position. If we do not have this position, we will generate duplicate by removing two ‘)’ in two steps only with a different order.
+After the removal, the prefix is then valid. We then call the function recursively to solve the rest of the string.
+However, we need to keep another information: the last removal position. If we do not have this position, we will
+generate duplicate by removing two ‘)’ in two steps only with a different order.
 For this, we keep tracking the last removal position and only remove ‘)’ after that.
 
 Now one may ask. What about ‘(‘? What if s = ‘(()(()’ in which we need remove ‘(‘?
@@ -67,13 +72,26 @@ public class RemoveInvalidParentheses {
             if (stack >= 0) continue;
             for (int j = last_j; j <= i; ++j)
                 if (s.charAt(j) == par[1] && (j == last_j || s.charAt(j - 1) != par[1]))
-                    remove(s.substring(0, j) + s.substring(j + 1, s.length()), ans, i, j, par);
+                    remove(s.substring(0, j) + s.substring(j + 1), ans, i, j, par);
             return;
         }
+        // when finishing from left to right, we reverse it.
+        // when finishing from right to left, we reverse it again.
         String reversed = new StringBuilder(s).reverse().toString();
         if (par[0] == '(') // finished left to right
             remove(reversed, ans, 0, 0, new char[]{')', '('});
         else // finished right to left
-            ans.add(reversed);
+            ans.add(reversed); // store the string in normal order.
+    }
+
+    public static void main(String[] args) {
+        //String s = "())())";
+        //String s = "())))";
+        String s = "()))((()"; // first from left to right, then from right to left
+        RemoveInvalidParentheses r = new RemoveInvalidParentheses();
+        List<String> res = r.removeInvalidParentheses(s);
+        for (String str : res) {
+            System.out.println(str);
+        }
     }
 }
