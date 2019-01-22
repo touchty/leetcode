934a6503fb0827adfa567f0c2cf83771c2361f23
Tao Yao <touchxyz@163.com>
2019-01-22 09:00:04 +0800

    496. Next Greater Element I

---
+++ src/main/java/array/NextGreaterElementI.java
@@ -0,0 +1,52 @@
+package array;
+
+import org.junit.Assert;
+
+import java.util.*;
+
+/**
+ * 496. Next Greater Element I
+ * Easy
+ * 659
+ * 1048
+ * Favorite
+ * Share
+ * You are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2. Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
+ * The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. If it does not exist, output -1 for this number.
+ * Example 1:
+ * Input: nums1 = [4,1,2], nums2 = [1,3,4,2].
+ * Output: [-1,3,-1]
+ * Explanation:
+ * For number 4 in the first array, you cannot find the next greater number for it in the second array, so output -1.
+ * For number 1 in the first array, the next greater number for it in the second array is 3.
+ * For number 2 in the first array, there is no next greater number for it in the second array, so output -1.
+ * Example 2:
+ * Input: nums1 = [2,4], nums2 = [1,2,3,4].
+ * Output: [3,-1]
+ * Explanation:
+ * For number 2 in the first array, the next greater number for it in the second array is 3.
+ * For number 4 in the first array, there is no next greater number for it in the second array, so output -1.
+ */
+public class NextGreaterElementI {
+    public static int[] nextGreaterElement(int[] findNums, int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>(); // map from x to next greater element of x
+        Deque<Integer> stack = new LinkedList<>();
+        for (int num : nums) {
+            // num : first greater num of previous elements
+            while (!stack.isEmpty() && stack.peek() < num)
+                map.put(stack.pop(), num);
+            stack.push(num);
+        }
+        for (int i = 0; i < findNums.length; i++)
+            findNums[i] = map.getOrDefault(findNums[i], -1);
+        return findNums;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {4, 1, 2};
+        int[] nums2 = {1, 3, 4, 2};
+        int[] res = NextGreaterElementI.nextGreaterElement(nums1, nums2);
+        int[] expected = {-1, 3, -1};
+        Assert.assertArrayEquals(expected, res);
+    }
+}
