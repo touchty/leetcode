fa910cb289dc5efee7d7f6a437e04670beee27f2
Tao Yao <touchxyz@163.com>
2019-02-23 09:56:14 +0800

    391. Perfect Rectangle

---
+++ src/main/java/array/PerfectRectangle.java
@@ -0,0 +1,83 @@
+package array;
+
+import java.util.HashSet;
+/*
+391. Perfect Rectangle
+Given N axis-aligned rectangles where N > 0, determine if they all together form an exact cover of a rectangular region.
+
+Each rectangle is represented as a bottom-left point and a top-right point. For example, a unit square is represented as [1,1,2,2]. (coordinate of bottom-left point is (1, 1) and top-right point is (2, 2)).
+
+
+Example 1:
+
+rectangles = [
+  [1,1,3,3],
+  [3,1,4,2],
+  [3,2,4,4],
+  [1,3,2,4],
+  [2,3,3,4]
+]
+
+Return true. All 5 rectangles together form an exact cover of a rectangular region.
+
+
+
+
+Example 2:
+
+rectangles = [
+  [1,1,2,3],
+  [1,3,2,4],
+  [3,1,4,2],
+  [3,2,4,4]
+]
+
+Return false. Because there is a gap between the two rectangular regions.
+ */
+/*
+Solution:
+The right answer must satisfy two conditions:
+the large rectangle area should be equal to the sum of small rectangles
+count of all the points should be even, and that of all the four corner points should be one
+ */
+public class PerfectRectangle {
+    public boolean isRectangleCover(int[][] rectangles) {
+
+        if (rectangles.length == 0 || rectangles[0].length == 0) return false;
+
+        int x1 = Integer.MAX_VALUE;
+        int x2 = Integer.MIN_VALUE;
+        int y1 = Integer.MAX_VALUE;
+        int y2 = Integer.MIN_VALUE;
+
+        HashSet<String> set = new HashSet<String>();
+        int area = 0;
+
+        for (int[] rect : rectangles) {
+            x1 = Math.min(rect[0], x1);
+            y1 = Math.min(rect[1], y1);
+            x2 = Math.max(rect[2], x2);
+            y2 = Math.max(rect[3], y2);
+
+            area += (rect[2] - rect[0]) * (rect[3] - rect[1]);
+
+            String s1 = rect[0] + " " + rect[1];
+            String s2 = rect[0] + " " + rect[3];
+            String s3 = rect[2] + " " + rect[3];
+            String s4 = rect[2] + " " + rect[1];
+
+            if (!set.add(s1)) set.remove(s1);
+            if (!set.add(s2)) set.remove(s2);
+            if (!set.add(s3)) set.remove(s3);
+            if (!set.add(s4)) set.remove(s4);
+        }
+        // overlap are not allowed!
+        if (!set.contains(x1 + " " + y1) || !set.contains(x1 + " " + y2) || !set.contains(x2 + " " + y1) || !set.contains(x2 + " " + y2) || set.size() != 4) return false;
+
+        return area == (x2-x1) * (y2-y1);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
