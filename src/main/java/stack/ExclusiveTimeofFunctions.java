5b6b5b3a76f0eea370e0e9ddd5dd2f7c06144864
Tao Yao <touchxyz@163.com>
2019-02-27 21:23:46 +0800

    636. Exclusive Time of Functions

---
+++ src/main/java/stack/ExclusiveTimeofFunctions.java
@@ -0,0 +1,78 @@
+package stack;
+
+import edu.princeton.cs.algs4.Stack;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+636. Exclusive Time of Functions
+Medium
+
+389
+
+603
+
+Favorite
+
+Share
+Given the running logs of n functions that are executed in a nonpreemptive single threaded CPU, find the exclusive time of these functions.
+
+Each function has a unique id, start from 0 to n-1. A function may be called recursively or by another function.
+
+A log is a string has this format : function_id:start_or_end:timestamp. For example, "0:start:0" means function 0 starts from the very beginning of time 0. "0:end:0" means function 0 ends to the very end of time 0.
+
+Exclusive time of a function is defined as the time spent within this function, the time spent by calling other functions should not be considered as this function's exclusive time. You should return the exclusive time of each function sorted by their function id.
+
+Example 1:
+Input:
+n = 2
+logs =
+["0:start:0",
+ "1:start:2",
+ "1:end:5",
+ "0:end:6"]
+Output:[3, 4]
+Explanation:
+Function 0 starts at time 0, then it executes 2 units of time and reaches the end of time 1.
+Now function 0 calls function 1, function 1 starts at time 2, executes 4 units of time and end at time 5.
+Function 0 is running again at time 6, and also end at the time 6, thus executes 1 unit of time.
+So function 0 totally execute 2 + 1 = 3 units of time, and function 1 totally execute 4 units of time.
+ */
+public class ExclusiveTimeofFunctions {
+    public static int[] exclusiveTime(int n, List<String> logs) {
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();//store id, not timestamp
+        int prev = 0;//store timestamp
+        for (String log : logs) {
+            String[] strs = log.split(":");
+            int id = Integer.parseInt(strs[0]);
+            int curr = Integer.parseInt(strs[2]);
+            if (strs[1].equals("start")) {
+                if (!stack.isEmpty()) {
+                    res[stack.peek()] += curr - prev;
+                }
+                stack.push(id);
+                prev = curr;
+            } else {
+                res[stack.pop()] += curr - prev + 1;
+                prev = curr + 1;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int n = 2;
+        String[] logs = {
+                "0:start:0",
+                "1:start:2",
+                "1:end:5",
+                "0:end:6"};
+        List<String> list = Arrays.asList(logs);
+        int[] durations = exclusiveTime(n, list);
+        for (int i: durations) {
+            System.out.println(i);
+        }
+    }
+}
