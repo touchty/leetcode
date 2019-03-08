c52afa032d93c6f1cb31b3e763fb610b2d1cd06b
Tao Yao <touchxyz@163.com>
2019-03-08 21:57:38 +0800

    575. Distribute Candies

---
+++ src/main/java/array/DistributeCandies.java
@@ -0,0 +1,32 @@
+package array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+575. Distribute Candies
+Easy
+
+268
+
+616
+
+Favorite
+
+Share
+Given an integer array with even length, where different numbers in this array represent different kinds of candies. Each number means one candy of the corresponding kind. You need to distribute these candies equally in number to brother and sister. Return the maximum number of kinds of candies the sister could gain.
+Example 1:
+Input: candies = [1,1,2,2,3,3]
+Output: 3
+Explanation:
+There are three different kinds of candies (1, 2 and 3), and two candies for each kind.
+Optimal distribution: The sister has candies [1,2,3] and the brother has candies [1,2,3], too.
+The sister has three different kinds of candies.
+ */
+public class DistributeCandies {
+    public int distributeCandies(int[] candies) {
+        Set<Integer> kinds = new HashSet<>();
+        for (int candy : candies) kinds.add(candy);
+        return kinds.size() >= candies.length / 2 ? candies.length / 2 : kinds.size();
+    }
+}
