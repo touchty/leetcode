8f0b1514c4a465efe7a5319242b2920a10f1f714
Tao Yao <touchxyz@163.com>
2019-03-18 09:23:25 +0800

    BestTimetoBuyandSellStockIII

--- src/main/java/dp/BestTimetoBuyandSellStockIII.java
+++ src/main/java/dp/BestTimetoBuyandSellStockIII.java
@@ -1,13 +1,13 @@
 package dp;
 
 public class BestTimetoBuyandSellStockIII {
-/*
-* Solution is commented in the code. Time complexity is O(kn),
-* space complexity can be O(n) because this DP only uses the
-* result from last step. But for cleaness this solution still
-* used O(kn) space complexity to
-* preserve similarity to the equations in the comments.
-* */
+    /*
+     * Solution is commented in the code. Time complexity is O(kn),
+     * space complexity can be O(n) because this DP only uses the
+     * result from last step. But for clearness this solution still
+     * used O(kn) space complexity to
+     * preserve similarity to the equations in the comments.
+     * */
 
     public int maxProfit(int[] prices) {
         // f[k, ii] represents the max profit up until prices[ii] (Note: NOT ending with prices[ii]) using at most k transactions.
@@ -32,25 +32,6 @@ public class BestTimetoBuyandSellStockIII {
         }
     }
 
-    int maxProfitRewrite(int[] prices) {
-        if (prices.length <= 1)
-            return 0;
-
-        int K = 2;
-        int maxProfit = 0;
-        int[][] f = new int[K + 1][prices.length];
-
-        for (int kk = 1; kk < f.length; kk++) {
-            int tmpMax = f[kk - 1][0] - prices[0];
-            for (int ii = 1; ii < prices.length; ii++) {
-                f[kk][ii] = Math.max(f[kk][ii - 1], prices[ii] + tmpMax);
-                tmpMax = Math.max(tmpMax, f[kk - 1][ii] - prices[ii]);
-                maxProfit = Math.max(f[kk][ii], maxProfit);
-            }
-        }
-        return maxProfit;
-    }
-
     int maxProfit1(int k, int[] prices) {
         int n = prices.length;
         if (n < 2) return 0;
@@ -73,6 +54,27 @@ public class BestTimetoBuyandSellStockIII {
             }
         }
         return f[k];
+    }
+
+    public static int MaxProfitDP(int[] prices) {
+        if (prices.length == 0) return 0;
+        int[][] dp = new int[3][prices.length];
+        for (int k = 1; k <= 2; k++) {
+            for (int i = 1; i < prices.length; i++) {
+                int min = prices[0];
+                for (int j = 1; j <= i; j++)
+                    min = Math.min(min, prices[j] - dp[k - 1][j - 1]);
+                dp[k][i] = Math.max(dp[k][i - 1], prices[i] - min);
+            }
+        }
+
+        return dp[2][prices.length - 1];
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {3, 3, 5, 0, 0, 3, 1, 4};
+        int maxProfit = BestTimetoBuyandSellStockIII.MaxProfitDP(prices);
+        System.out.println(maxProfit);
     }
 }
 
