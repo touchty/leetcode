c0b80c9d648d1b43ff0c64856360362439d94650
touchty <touchxyz@163.com>
2019-03-21 22:44:20 +0800

    122. Best Time to Buy and Sell Stock II

--- src/main/java/dp/BestTimetoBuyandSellStockII.java
+++ src/main/java/dp/BestTimetoBuyandSellStockII.java
@@ -1,14 +1,67 @@
 package dp;
 
+/*
+122. Best Time to Buy and Sell Stock II
+Easy
+
+865
+
+1214
+
+Favorite
+
+Share
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+
+Example 1:
+
+Input: [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+             Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Example 2:
+
+Input: [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+             Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
+             engaging multiple transactions at the same time. You must sell before buying again.
+Example 3:
+
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ */
 public class BestTimetoBuyandSellStockII {
-    public int maxProfit(int[] prices){
+    public int maxProfit(int[] prices) {
         int max = 0;
         for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i-1]){
-                max += prices[i] - prices[i-1];
+            if (prices[i] > prices[i - 1]) {
+                // if prices[i] <=  prices[i-1], there is no such operation.
+                // (prices[i] - prices[i-1]) + (prices[i-1] - prices[i-2]) + ... + (prices[k] - prices[k - 1])
+                // == prices[i] - prices[k - 1]
+                // on condition that prices[i]  > prices[i-1] > prices[i - 2] > ... > prices[k] > prices[k-1]
+                max += prices[i] - prices[i - 1];
             }
         }
         return max;
+    }
+
+    public int maxProfitOpt(int[] prices) {
+        int profit = 0, i = 0;
+        while (i < prices.length) {
+            // find next local minimum
+            while (i < prices.length - 1 && prices[i + 1] <= prices[i]) i++;
+            int min = prices[i++]; // need increment to avoid infinite loop for "[1]"
+            // find next local maximum
+            while (i < prices.length - 1 && prices[i + 1] >= prices[i]) i++;
+            profit += i < prices.length ? prices[i++] - min : 0;
+        }
+        return profit;
     }
 
 }
