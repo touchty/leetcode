d8ddfb9e8222164bf735b628a15d899fc3edfd24
touchty <touchxyz@163.com>
2019-03-25 11:14:07 +0800

    123. Best Time to Buy and Sell Stock III

--- src/main/java/dp/BestTimetoBuyandSellStockIII.java
+++ src/main/java/dp/BestTimetoBuyandSellStockIII.java
@@ -47,22 +47,23 @@ public class BestTimetoBuyandSellStockIII {
      * */
 
     public int maxProfit(int[] prices) {
-        // f[k, ii] represents the max profit up until prices[ii] (Note: NOT ending with prices[ii]) using at most k transactions.
-        // f[k, ii] = max(f[k, ii-1], prices[ii] - prices[jj] + f[k-1, jj]) { jj in range of [0, ii-1] }
-        //          = max(f[k, ii-1], prices[ii] + max(f[k-1, jj] - prices[jj]))
-        // f[0, ii] = 0; 0 times transation makes 0 profit
+        // f[k, i] represents the max profit up until prices[i] (Note: NOT ending with prices[i]) using at most k
+        // transactions.
+        // f[k, i] = max(f[k, i-1], prices[i] - prices[j] + f[k-1, j]) { j in range of [0, i-1] }
+        //          = max(f[k, i-1], prices[i] + max(f[k-1, j] - prices[j]))
+        // f[0, i] = 0; 0 times transation makes 0 profit
         // f[k, 0] = 0; if there is only one price data point you can't make any money no matter how many times you can trade
         if (prices.length <= 1) return 0;
         else {
             int K = 2; // number of max transation allowed
             int maxProf = 0;
             int[][] f = new int[K + 1][prices.length];
-            for (int kk = 1; kk <= K; kk++) {
-                int tmpMax = f[kk - 1][0] - prices[0];
-                for (int ii = 1; ii < prices.length; ii++) {
-                    f[kk][ii] = Math.max(f[kk][ii - 1], prices[ii] + tmpMax);
-                    tmpMax = Math.max(tmpMax, f[kk - 1][ii] - prices[ii]);
-                    maxProf = Math.max(f[kk][ii], maxProf);
+            for (int k = 1; k <= K; k++) {
+                int tmpMax = f[k - 1][0] - prices[0];
+                for (int i = 1; i < prices.length; i++) {
+                    f[k][i] = Math.max(f[k][i - 1], prices[i] + tmpMax);
+                    tmpMax = Math.max(tmpMax, f[k - 1][i] - prices[i]);
+                    maxProf = Math.max(f[k][i], maxProf);
                 }
             }
             return maxProf;
