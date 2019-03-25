3a8fbf1ff80302b3f2716b4f7bef9a83a5cca21a
touchty <touchxyz@163.com>
2019-03-25 11:08:16 +0800

    123. Best Time to Buy and Sell Stock III

--- src/main/java/dp/BestTimetoBuyandSellStockIII.java
+++ src/main/java/dp/BestTimetoBuyandSellStockIII.java
@@ -1,5 +1,42 @@
 package dp;
 
+/*
+123. Best Time to Buy and Sell Stock III
+Hard
+
+929
+
+53
+
+Favorite
+
+Share
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete at most two transactions.
+
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+
+Example 1:
+
+Input: [3,3,5,0,0,3,1,4]
+Output: 6
+Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+             Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+Example 2:
+
+Input: [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+             Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
+             engaging multiple transactions at the same time. You must sell before buying again.
+Example 3:
+
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ */
+
 public class BestTimetoBuyandSellStockIII {
     /*
      * Solution is commented in the code. Time complexity is O(kn),
@@ -32,30 +69,6 @@ public class BestTimetoBuyandSellStockIII {
         }
     }
 
-    int maxProfit1(int k, int[] prices) {
-        int n = prices.length;
-        if (n < 2) return 0;
-        if (k >= n / 2) { // buy-sell-II case, unlimited
-            int maxProfit = 0;
-            for (int i = 1; i < n; ++i) {
-                if (prices[i] > prices[i - 1]) maxProfit += prices[i] - prices[i - 1];
-            }
-            return maxProfit;
-        }
-        // if k<n/2, use DP approach but only O(k) space
-        int[] f = new int[k + 1];
-        int[] g = new int[k + 1];
-        for (int i = 1; i < n; ++i) {
-            int diff = prices[i] - prices[i - 1], temp = f[0];
-            for (int kk = 1; kk <= k; ++kk) {
-                g[kk] = Math.max(g[kk], temp) + diff;
-                temp = f[kk];
-                f[kk] = Math.max(f[kk], g[kk]);
-            }
-        }
-        return f[k];
-    }
-
     public static int MaxProfitDP(int[] prices) {
         if (prices.length == 0) return 0;
         int[][] dp = new int[3][prices.length];
@@ -69,6 +82,18 @@ public class BestTimetoBuyandSellStockIII {
         }
 
         return dp[2][prices.length - 1];
+    }
+
+    public int maxProfitOpt(int[] prices) {
+        int hold1 = Integer.MIN_VALUE, hold2 = Integer.MIN_VALUE;
+        int release1 = 0, release2 = 0;
+        for(int i:prices){                              // Assume we only have 0 money at first
+            release2 = Math.max(release2, hold2+i);     // The maximum if we've just sold 2nd stock so far.
+            hold2    = Math.max(hold2,    release1-i);  // The maximum if we've just buy  2nd stock so far.
+            release1 = Math.max(release1, hold1+i);     // The maximum if we've just sold 1nd stock so far.
+            hold1    = Math.max(hold1,    -i);          // The maximum if we've just buy  1st stock so far.
+        }
+        return release2; ///Since release1 is initiated as 0, so release2 will always higher than release1.
     }
 
     public static void main(String[] args) {
