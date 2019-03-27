9f6052d3e63645379e4ffc6ed2f9f4e11df81eef
touchty <touchxyz@163.com>
2019-03-27 09:56:35 +0800

    701. Insert into a Binary Search Tree

---
+++ src/main/java/tree/InsertintoaBinarySearchTree.java
@@ -0,0 +1,63 @@
+package tree;
+
+/*
+701. Insert into a Binary Search Tree
+Medium
+
+273
+
+30
+
+Favorite
+
+Share
+Given the root node of a binary search tree (BST) and a value to be inserted into the tree, insert the value into the BST. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
+
+Note that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
+
+For example,
+
+Given the tree:
+        4
+       / \
+      2   7
+     / \
+    1   3
+And the value to insert: 5
+You can return this binary search tree:
+
+         4
+       /   \
+      2     7
+     / \   /
+    1   3 5
+This tree is also valid:
+
+         5
+       /   \
+      2     7
+     / \
+    1   3
+         \
+          4
+ */
+public class InsertintoaBinarySearchTree {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+        if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        }
+        else{
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(3);
+        InsertintoaBinarySearchTree solution = new InsertintoaBinarySearchTree();
+        root = solution.insertIntoBST(root, 5);
+    }
+}
