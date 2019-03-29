7d8d1456716a30d032c1fcab930e3eec0da2e747
touchty <touchxyz@163.com>
2019-03-29 10:30:03 +0800

    939. Minimum Area Rectangle

---
+++ src/main/java/array/MinimumAreaRectangle.java
@@ -0,0 +1,63 @@
+package array;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/*
+939. Minimum Area Rectangle
+Medium
+
+186
+
+38
+
+Favorite
+
+Share
+Given a set of points in the xy-plane, determine the minimum area of a rectangle formed from these points, with sides parallel to the x and y axes.
+
+If there isn't any rectangle, return 0.
+
+
+
+Example 1:
+
+Input: [[1,1],[1,3],[3,1],[3,3],[2,2]]
+Output: 4
+Example 2:
+
+Input: [[1,1],[1,3],[3,1],[3,3],[4,1],[4,3]]
+Output: 2
+ */
+public class MinimumAreaRectangle {
+    public int minAreaRect(int[][] points) {
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        for (int[] p : points) {
+            if (!map.containsKey(p[0])) {
+                map.put(p[0], new HashSet<>());
+            }
+            map.get(p[0]).add(p[1]);
+        }
+        int min = Integer.MAX_VALUE;
+        for (int[] p1 : points) {
+            for (int[] p2 : points) {
+                if (p1[0] == p2[0] || p1[1] == p2[1]) { // if have the same x or y
+                    continue;
+                }
+                if (map.get(p1[0]).contains(p2[1]) && map.get(p2[0]).contains(p1[1])) { // find other two points
+                    min = Math.min(min, Math.abs(p1[0] - p2[0]) * Math.abs(p1[1] - p2[1]));
+                }
+            }
+        }
+        return min == Integer.MAX_VALUE ? 0 : min;
+    }
+
+    public static void main(String[] args) {
+        int[][] points = {{1, 1}, {1, 3}, {3, 1}, {3, 3}, {2, 2}};
+        MinimumAreaRectangle solution = new MinimumAreaRectangle();
+        int minRect = solution.minAreaRect(points);
+        System.out.println(minRect);
+    }
+}
