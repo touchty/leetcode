9686a8eacd9647c5838884b956e3f64e0ed32648
touchty <touchxyz@163.com>
2019-03-29 10:10:11 +0800

    998. Maximum Binary Tree II

---
+++ src/main/java/tree/MaximumBinaryTreeII.java
@@ -0,0 +1,53 @@
+package tree;
+
+/*
+998. Maximum Binary Tree II
+Medium
+
+57
+
+103
+
+Favorite
+
+Share
+We are given the root node of a maximum tree: a tree where every node has a value greater than any other value in its subtree.
+
+Just as in the previous problem, the given tree was constructed from an list A (root = Construct(A)) recursively with the following Construct(A) routine:
+
+If A is empty, return null.
+Otherwise, let A[i] be the largest element of A.  Create a root node with value A[i].
+The left child of root will be Construct([A[0], A[1], ..., A[i-1]])
+The right child of root will be Construct([A[i+1], A[i+2], ..., A[A.length - 1]])
+Return root.
+Note that we were not given A directly, only a root node root = Construct(A).
+
+Suppose B is a copy of A with the value val appended to it.  It is guaranteed that B has unique values.
+
+Return Construct(B).
+
+
+
+Example 1:
+
+
+
+Input: root = [4,1,3,null,null,2], val = 5
+Output: [5,4,null,1,3,null,null,2]
+Explanation: A = [1,4,2,3], B = [1,4,2,3,5]
+ */
+public class MaximumBinaryTreeII {
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (val > root.val) {
+            TreeNode node = new TreeNode(val);
+            node.left = root;
+            return node;
+        } else if (val < root.val) {
+            root.right = insertIntoMaxTree(root.right, val);
+        }
+        return root;
+    }
+}
