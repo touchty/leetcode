1cd008debe21e6254da31b403516f207a66fea5d
touchty <touchxyz@163.com>
2019-03-29 10:37:39 +0800

    939. Minimum Area Rectangle

--- src/main/java/array/MinimumAreaRectangle.java
+++ src/main/java/array/MinimumAreaRectangle.java
@@ -38,7 +38,7 @@ public class MinimumAreaRectangle {
             if (!map.containsKey(p[0])) {
                 map.put(p[0], new HashSet<>());
             }
-            map.get(p[0]).add(p[1]);
+            map.get(p[0]).add(p[1]); // (x: y)
         }
         int min = Integer.MAX_VALUE;
         for (int[] p1 : points) {
@@ -46,6 +46,9 @@ public class MinimumAreaRectangle {
                 if (p1[0] == p2[0] || p1[1] == p2[1]) { // if have the same x or y
                     continue;
                 }
+                /*
+                find the two diagonal corners of rectangle
+                 */
                 if (map.get(p1[0]).contains(p2[1]) && map.get(p2[0]).contains(p1[1])) { // find other two points
                     min = Math.min(min, Math.abs(p1[0] - p2[0]) * Math.abs(p1[1] - p2[1]));
                 }
