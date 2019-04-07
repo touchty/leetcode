0ee4be7da5f632d4d5a08180911df4422608953b
touchty <touchxyz@163.com>
2019-04-07 17:20:26 +0800

    857. Minimum Cost to Hire K Workers

---
+++ src/main/java/array/MinimumCosttoHireKWorkers.java
@@ -0,0 +1,76 @@
+package array;
+
+import org.junit.Assert;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+/*
+857. Minimum Cost to Hire K Workers
+Hard
+
+465
+
+45
+
+Favorite
+
+Share
+There are N workers.  The i-th worker has a quality[i] and a minimum wage expectation wage[i].
+
+Now we want to hire exactly K workers to form a paid group.  When hiring a group of K workers, we must pay them according to the following rules:
+
+Every worker in the paid group should be paid in the ratio of their quality compared to other workers in the paid group.
+Every worker in the paid group must be paid at least their minimum wage expectation.
+Return the least amount of money needed to form a paid group satisfying the above conditions.
+
+
+
+Example 1:
+
+Input: quality = [10,20,5], wage = [70,50,30], K = 2
+Output: 105.00000
+Explanation: We pay 70 to 0-th worker and 35 to 2-th worker.
+Example 2:
+
+Input: quality = [3,1,10,10,1], wage = [4,8,2,2,7], K = 3
+Output: 30.66667
+Explanation: We pay 4 to 0-th worker, 13.33333 to 2-th and 3-th workers seperately.
+
+
+Note:
+
+1 <= K <= N <= 10000, where N = quality.length = wage.length
+1 <= quality[i] <= 10000
+1 <= wage[i] <= 10000
+Answers within 10^-5 of the correct answer will be considered correct.
+ */
+public class MinimumCosttoHireKWorkers {
+    public double mincostToHireWorkers(int[] q, int[] w, int K) {
+        double[][] workers = new double[q.length][2];
+        for (int i = 0; i < q.length; ++i)
+            workers[i] = new double[]{(double) (w[i]) / q[i], (double) q[i]};
+        Arrays.sort(workers, (a, b) -> Double.compare(a[0], b[0]));
+        double res = Double.MAX_VALUE, qsum = 0;
+        PriorityQueue<Double> pq = new PriorityQueue<>();
+        for (double[] worker : workers) {
+            qsum += worker[1];
+            pq.add(-worker[1]);
+            if (pq.size() > K) qsum += pq.poll();
+            if (pq.size() == K) res = Math.min(res, qsum * worker[0]);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] quality = {10, 20, 5};
+        int[] wage = {70, 50, 30};
+        int K = 2;
+        MinimumCosttoHireKWorkers solution = new MinimumCosttoHireKWorkers();
+        double totalWages = solution.mincostToHireWorkers(quality, wage, K);
+        double expected = 105.00;
+        Assert.assertTrue(expected - totalWages < 1.0E-20);
+        String str = "hello";
+        System.out.println(str.hashCode());
+    }
+}
