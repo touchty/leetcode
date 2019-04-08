5a2dfaafcee6df033c8067f189bbf5eda4535a6d
touchty <touchxyz@163.com>
2019-04-08 09:32:12 +0800

    857. Minimum Cost to Hire K Workers

--- src/main/java/array/MinimumCosttoHireKWorkers.java
+++ src/main/java/array/MinimumCosttoHireKWorkers.java
@@ -45,6 +45,38 @@ Note:
 1 <= wage[i] <= 10000
 Answers within 10^-5 of the correct answer will be considered correct.
  */
+
+
+/*
+solution: lee215
+https://leetcode.com/problems/minimum-cost-to-hire-k-workers/discuss/141768/Detailed-explanation-O(NlogN)
+Let's read description first and figure out the two rules:
+
+"1. Every worker in the paid group should be paid in the ratio of their quality compared to other workers in the paid group."
+So for any two workers in the paid group,
+we have wage[i] : wage[j] = quality[i] : quality[j]
+So we have wage[i] : quality[i] = wage[j] : quality[j]
+We pay wage to every worker in the group with the same ratio compared to his own quality.
+
+"2. Every worker in the paid group must be paid at least their minimum wage expectation."
+For every worker, he has an expected ratio of wage compared to his quality.
+
+So to minimize the total wage, we want a small ratio.
+So we sort all workers with their expected ratio, and pick up K first worker.
+Now we have a minimum possible ratio for K worker and we their total quality.
+
+As we pick up next worker with bigger ratio, we increase the ratio for whole group.
+Meanwhile we remove a worker with highest quality so that we keep K workers in the group.
+We calculate the current ratio * total quality = total wage for this group.
+
+We redo the process and we can find the minimum total wage.
+Because workers are sorted by ratio of wage/quality.
+For every ratio, we find the minimum possible total quality of K workers.
+
+Time Complexity
+O(NlogN) for sort.
+O(NlogK) for priority queue.
+ */
 public class MinimumCosttoHireKWorkers {
     public double mincostToHireWorkers(int[] q, int[] w, int K) {
         double[][] workers = new double[q.length][2];
