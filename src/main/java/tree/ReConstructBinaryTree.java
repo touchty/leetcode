cb958951a3769a982ba4cc45864afb7fe16775bc
touchty <touchxyz@163.com>
2019-04-16 23:59:10 +0800

    ReConstructBinaryTree

---
+++ src/main/java/tree/ReConstructBinaryTree.java
@@ -0,0 +1,33 @@
+package tree;
+
+public class ReConstructBinaryTree {
+    public static TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+        TreeNode root = reConstructBinaryTree(pre, 0, pre.length - 1, in, 0, in.length - 1);
+        return root;
+    }
+
+    //前序遍历{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}
+    private static TreeNode reConstructBinaryTree(int[] pre, int startPre, int endPre, int[] in, int startIn, int endIn) {
+
+        if (startPre > endPre || startIn > endIn)
+            return null;
+        TreeNode root = new TreeNode(pre[startPre]);
+
+        for (int i = startIn; i <= endIn; i++)
+            if (in[i] == pre[startPre]) {
+                root.left = reConstructBinaryTree(pre, startPre + 1, startPre + i - startIn, in, startIn, i - 1);
+                root.right = reConstructBinaryTree(pre, i - startIn + startPre + 1, endPre, in, i + 1, endIn);
+            }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        int[] pre = {1, 2, 4, 7, 3, 5, 6, 8};
+        int[] in = {4, 7, 2, 1, 5, 3, 8, 6};
+        TreeNode root = ReConstructBinaryTree.reConstructBinaryTree(pre, in);
+        System.out.println(root.val);
+        System.out.println(root.left.val);
+        System.out.println(root.right.val);
+    }
+}
