18aba7259f3e03427e6fb61ab20d589c442187d4
touchty <touchxyz@163.com>
2019-04-17 11:07:25 +0800

    MergekSortedLists

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,16 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {1};
-
-        int pos1 = Arrays.binarySearch(nums, 1);
-        int pos2 = Arrays.binarySearch(nums, 2);
-        int pos3 = Arrays.binarySearch(nums, 3);
-
-        System.out.println(pos1);
-        System.out.println(pos2);
-        System.out.println(pos3);
+        int n = 3;
+        int minus = -1;
+        System.out.println(Integer.toBinaryString(minus));
+        int mask = 1;
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((mask & minus) != 0)
+                res++;
+            mask = mask << 1;
+        }
+        System.out.println(res);
     }
 }
