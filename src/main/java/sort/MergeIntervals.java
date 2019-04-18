30cecb3fb40c89296e2b65eb291373869c74a005
touchty <touchxyz@163.com>
2019-04-18 10:21:50 +0800

    MergeIntervals

--- src/main/java/sort/MergeIntervals.java
+++ src/main/java/sort/MergeIntervals.java
@@ -1,5 +1,6 @@
 package sort;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -28,5 +29,18 @@ public class MergeIntervals {
         // Add the last interval
         result.add(new Interval(start, end));
         return result;
+    }
+
+    public static void main(String[] args) {
+        Interval i1 = new Interval(1, 3);
+        Interval i2 = new Interval(2, 4);
+        Interval i3 = new Interval(4, 6);
+        List<Interval> list = new ArrayList<>();
+        list.add(i1);
+        list.add(i2);
+        list.add(i3);
+        List<Interval> res = new MergeIntervals().merge(list);
+        for (Interval i : res)
+            System.out.println(i.start + " -> " + i.end);
     }
 }
