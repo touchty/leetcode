7d98a3a1359a58bf4756bdf09bf3c2eb6f5730ab
touchty <touchxyz@163.com>
2019-04-24 22:16:58 +0800

    SkipList

---
+++ src/main/java/algorithms/SkipListIterator.java
@@ -0,0 +1,33 @@
+package algorithms;
+
+/******************************************************************************
+ * SkipListIterator                                                            *
+ *                                                                             *
+ * View README file for information about this project.                        *
+ * View LICENSE file for license information.                                  *
+ ******************************************************************************/
+
+import java.util.*;
+
+public class SkipListIterator<E extends Comparable<E>> implements Iterator<E> {
+    SkipList<E> list;
+    SkipListNode<E> current;
+
+    public SkipListIterator(SkipList<E> list) {
+        this.list = list;
+        this.current = list.getHead();
+    }
+
+    public boolean hasNext() {
+        return current.nextNodes.get(0) != null;
+    }
+
+    public E next() {
+        current = (SkipListNode<E>)current.nextNodes.get(0);
+        return (E)current.getValue();
+    }
+
+    public void remove() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException();
+    }
+}
