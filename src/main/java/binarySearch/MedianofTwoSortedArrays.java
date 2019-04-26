ee1135231805ea32d206b81c638e34f55ee890b6
touchty <touchxyz@163.com>
2019-04-26 10:32:39 +0800

    Median of Two Sorted Arrays

--- src/main/java/binarySearch/MedianofTwoSortedArrays.java
+++ src/main/java/binarySearch/MedianofTwoSortedArrays.java
@@ -31,6 +31,8 @@ nums2 = [3, 4]
 The median is (2 + 3)/2 = 2.5
  */
 
+import org.junit.Assert;
+
 public class MedianofTwoSortedArrays {
     double findMedianSortedArrays(int A[], int m, int B[], int n) {
         int l = (m + n + 1) >> 1;
@@ -48,9 +50,9 @@ public class MedianofTwoSortedArrays {
      * @param k      第k小的数
      * @return 第k小的数
      */
-    int getkth(int A[], int startA, int m, int[] B, int startB, int n, int k) {
+    public int getkth(int A[], int startA, int m, int[] B, int startB, int n, int k) {
         // startBet m <= n
-        if (k > m + m || k <= 0) return Integer.MIN_VALUE;
+        if (k > m + n || k <= 0) return Integer.MIN_VALUE;
         if (m > n)
             return getkth(B, startB, n, A, startA, m, k);
         if (m == 0)
@@ -65,6 +67,15 @@ public class MedianofTwoSortedArrays {
             return getkth(A, startA + i, m - i, B, startB, n, k - i);
     }
 
+    /**
+     * @param A sorted array
+     * @param B sorted array
+     * @param k k >= 1
+     * @return kth smallest
+     */
+    int getKth(int[] A, int[] B, int k) {
+        return getkth(A, 0, A.length, B, 0, B.length, k);
+    }
 
     public static void main(String[] args) {
         int[] A = {1, 3, 6, 9};
@@ -73,6 +84,29 @@ public class MedianofTwoSortedArrays {
         double median = solution.findMedianSortedArrays(A, A.length, B, B.length);
         //System.out.println(median);
         int posof2 = solution.getkth(A, 0, A.length, B, 0, B.length, 100);
-        System.out.println(posof2);
+        //System.out.println(posof2);
+
+        // test getKth()
+        int N = 1000;
+        int[] evens = new int[N / 2];
+        int[] odds = new int[N / 2];
+        for (int i = 0; i < evens.length; i++) {
+            evens[i] = 2 * i;
+        }
+        for (int i = 0; i < evens.length; i++) {
+            odds[i] = 2 * i + 1;
+        }
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            //arr[i] = solution.getkth(evens, 0, evens.length, odds, 0, odds.length, i + 1);
+            arr[i] = solution.getKth(evens, odds, i + 1);
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+            if (i > 0 && arr[i] < arr[i - 1])
+                Assert.assertEquals(true, false);
+        }
     }
 }
