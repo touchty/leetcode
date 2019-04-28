daef981c0fcb7edebb1c3ed4b37e4e1033a7e228
touchty <touchxyz@163.com>
2019-04-28 23:48:51 +0800

    223. Rectangle Area

--- src/main/java/array/RectangleArea.java
+++ src/main/java/array/RectangleArea.java
@@ -1,6 +1,19 @@
 package array;
 
 /**
+ * 223. Rectangle Area
+ * Medium
+ *
+ * 235
+ *
+ * 452
+ *
+ * Favorite
+ *
+ * Share
+ * Find the total area covered by two rectilinear rectangles in a 2D plane.
+ *
+ * Each rectangle is defined by its bottom left corner and top right corner as shown in the figure.
  * Find the total area covered by two rectilinear rectangles in a 2D plane.
  * Each rectangle is defined by its bottom left corner and top right corner as shown in the figure.
  * Rectangle Area
@@ -14,7 +27,7 @@ package array;
  * Output: 45
  */
 public class RectangleArea {
-    public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+    public static int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
         int areaA = (C - A) * (D - B);
         int areaB = (G - E) * (H - F);
 
@@ -28,5 +41,11 @@ public class RectangleArea {
             overlap = (right - left) * (top - bottom);
         }
         return (areaA + areaB - overlap);
+    }
+
+    public static void main(String[] args) {
+        int A = -3, B = 0, C = 3, D = 4, E = 0, F = -1, G = 9, H = 2;
+        int area = RectangleArea.computeArea(A, B, C, D, E, F, G, H);
+        System.out.println(area);
     }
 }
