5e53bc597bd36ce6040c923a2cf9cdcd59ea9d92
touchty <touchxyz@163.com>
2019-04-28 16:13:21 +0800

    SudokuSolver

--- src/main/java/backtracking/SudokuSolver.java
+++ src/main/java/backtracking/SudokuSolver.java
@@ -38,4 +38,27 @@ public class SudokuSolver {
         }
         return true;
     }
+
+    public static void main(String[] args) {
+        char[][] b1 = {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+
+        SudokuSolver sudokuSolver = new SudokuSolver();
+        sudokuSolver.solveSudoku(b1);
+        for (int i = 0; i < b1.length; i++) {
+            for (int j = 0; j < b1[0].length; j++) {
+                System.out.printf(b1[i][j] + ", ");
+            }
+            System.out.println("");
+        }
+    }
 }
