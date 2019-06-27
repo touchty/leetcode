12e5b31fd3171af984709814e4db10a291d2c655
Tao Yao <touchxyz@163.com>
2019-06-27 22:27:58 +0800

    PalindromePairs

--- src/main/java/trie/PalindromePairs.java
+++ src/main/java/trie/PalindromePairs.java
@@ -11,11 +11,28 @@ import java.util.*;
     n2 (t)    n3 (f)
                 | 'a'
               n4 (t)
-1. For word "ba", starting from the first character 'b', index into the root.next array with index given by 'b' - 'a' = 1.
+1. For word "ba", starting from the first character 'b', index into the root.next array with index given
+by 'b' - 'a' = 1.
 The corresponding node is null, then we know there are no words ending at this character, so the searching process is
 terminated;
 
-2. For word "a", again indexing into array root.next at index given by 'a' - 'a' = 0 will yield node n1, which is not null. We then check the value of n1.isWord. If it is true, then it is possible to obtain a palindrome by appending this word to the one currently being examined (a.k.a word "a"). Also note that the two words should be distinct from each other, but the n1.isWord field provides no information about the word itself, which makes it impossible to distinguish the two words. So it is necessary to modify the structure of the TrieNode so that we can identify the word it represents. One easy way is to have an integer field to remember the index of the word in the words array. For non-word nodes, this integer will take negative values (-1 for example) while for those representing a word, it will be non-negative values. Suppose we have made this modification, then the two words will be identified to be the same, so we discard this pair combination. Since the word "a" has only one letter, it seems we are done with it. Or do we? Not really. What if there are words with suffix "a" ("aaa" in this case)? We need to continue to check the rest part of these words (such as "aa" for the word "aaa") and see if the rest forms a palindrome. If it is, then appending this word ("aaa" in this case) to the original word ("a") will also form a palindrome ("aaaa"). Here I take another strategy: add an integer list to each TrieNode; the list will record the indices of all words satisfying the following two conditions: 1. each word has a suffix represented by the current TrieNode; 2. the rest of the word forms a palindrome.
+2. For word "a", again indexing into array root.next at index given by 'a' - 'a' = 0 will yield node n1, which is
+not null. We then check the value of n1.isWord. If it is true, then it is possible to obtain a palindrome by appending
+this word to the one currently being examined (a.k.a word "a"). Also note that the two words should be distinct from
+each other, but the n1.isWord field provides no information about the word itself, which makes it impossible to
+distinguish the two words. So it is necessary to modify the structure of the TrieNode so that we can identify the
+word it represents. One easy way is to have an integer field to remember the index of the word in the words array.
+For non-word nodes, this integer will take negative values (-1 for example) while for those representing a word,
+it will be non-negative values. Suppose we have made this modification, then the two words will be identified to be
+the same, so we discard this pair combination. Since the word "a" has only one letter, it seems we are done with it.
+Or do we? Not really. What if there are words with suffix "a" ("aaa" in this case)? We need to continue to check
+the rest part of these words (such as "aa" for the word "aaa") and see if the rest forms a palindrome. If it is,
+then appending this word ("aaa" in this case) to the original word ("a") will also form a palindrome ("aaaa"). Here
+I take another strategy: add an integer list to each TrieNode;
+
+the list will record the indices of all words
+satisfying the following two conditions: 1. each word has a suffix represented by the current TrieNode;
+2. the rest of the word forms a palindrome.
 
           root (-1,[1,2])
             | 'a'
@@ -29,6 +46,16 @@ Here I take another strategy: add an integer list to each TrieNode; the list wil
 satisfying the following two conditions: 1. each word has a suffix represented by the current TrieNode; 2. the rest of
 the word forms a palindrome.
 */
+
+/*
+s1=abc
+s2=ddcba
+s1 + s2 == Palindrome
+
+s2=abc
+s1=ddcba
+s2 + s1 == Palindrome
+ */
 public class PalindromePairs {
     private static class TrieNode {
         TrieNode[] next;
@@ -82,7 +109,7 @@ public class PalindromePairs {
     private void search(String[] words, int i, TrieNode root, List<List<Integer>> res) {
         for (int j = 0; j < words[i].length(); j++) {
             if (root.index >= 0 && root.index != i && isPalindrome(words[i], j, words[i].length() - 1)) {
-                res.add(Arrays.asList(i, root.index));
+                res.add(Arrays.asList(i, root.index)); // words[i]+ words[index]
             }
 
             root = root.next[words[i].charAt(j) - 'a'];
@@ -111,7 +138,7 @@ public class PalindromePairs {
 //            //System.out.println(pair);
 //        }
 
-        words = new String[]{"abc", "defcba"};
+        words = new String[]{"abc", "cba"};
         pairs = new PalindromePairs().palindromePairs(words);
         Collections.sort(pairs, Comparator.comparingInt(a -> a.get(0)));
         for (List<Integer> pair : pairs) {
