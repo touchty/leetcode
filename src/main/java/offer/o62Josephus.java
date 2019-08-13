26cadd9cde4a47ee4d59f996406bc9ea84318b39
touchty <touchxyz@163.com>
2019-08-14 00:19:46 +0800

    OFFER!

---
+++ src/main/java/offer/o62Josephus.java
@@ -0,0 +1,13 @@
+package offer;
+
+
+// 约瑟夫环问题(Josephus)
+public class o62Josephus {
+    public int LastRemaining_Solution(int n, int m) {
+        if (n == 0)     /* 特殊输入的处理 */
+            return -1;
+        if (n == 1)     /* 递归返回条件 */
+            return 0;
+        return (LastRemaining_Solution(n - 1, m) + m) % n;
+    }
+}
