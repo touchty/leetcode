626c72ae9d32caf9240c4449f6ca14b7d407f145
touchty <touchxyz@163.com>
2019-08-22 11:34:18 +0800

    鸽巢原理，找到出现次数大于一半的元素

--- src/main/java/offer/O55_1TreeDepth.java
+++ src/main/java/offer/O55_1TreeDepth.java
@@ -2,8 +2,48 @@ package offer;
 
 import tree.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class O55_1TreeDepth {
     public int TreeDepth(TreeNode root) {
         return root == null ? 0 : 1 + Math.max(TreeDepth(root.left), TreeDepth(root.right));
+    }
+
+    private int width = 0;
+
+    public int treeWidth(TreeNode root) {
+        if (root == null)
+            return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            this.width = Math.max(this.width, size);
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null)
+                    queue.offer(node.left);
+                if (node.right != null)
+                    queue.offer(node.right);
+            }
+        }
+        return this.width;
+    }
+
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+
+        root.right = new TreeNode(2);
+        root.right.right = new TreeNode(3);
+
+        O55_1TreeDepth solution = new O55_1TreeDepth();
+        int depth = solution.TreeDepth(root);
+        int width = solution.treeWidth(root);
+        System.out.println(depth);
+        System.out.println(width);
     }
 }
