9216cfaa44fe0f379ca979341ba3a6e7ab619b93
touchty <touchxyz@163.com>
2019-08-25 20:53:46 +0800

    1

--- src/main/java/ZJTD/Main1.java
+++ src/main/java/ZJTD/Main1.java
@@ -5,17 +5,11 @@ import java.util.Scanner;
 public class Main1 {
     static int component(int[][] nums, int n) {
         int components = 0;
-        boolean[][] v = new boolean[n][n];
+        boolean[] v = new boolean[n];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == j) {
-                    components++;
-                    v[i][j] = true;
-                }
-                if (!v[i][j] && nums[i][j] >= 3) {
-                    components++;
-                    dfs(nums, v, i, j);
-                }
+            if (!v[i]) {
+                components++;
+                dfs(nums, v, i);
             }
         }
 
@@ -41,7 +35,7 @@ public class Main1 {
         }
     }*/
 
-    static void dfs(int[][] nums, boolean[][] v, int i, int j) {
+    /*static void dfs(int[][] nums, boolean[][] v, int i, int j) {
         if (i < 0 || i >= nums.length || j < 0 || j >= nums[0].length || v[i][j])
             return;
         v[i][j] = true;
@@ -54,19 +48,35 @@ public class Main1 {
             if (nums[i][k] >= 3)
                 dfs(nums, v, i, k);
         }
+    }*/
+    static void dfs(int[][] nums, boolean[] v, int i) {
+        if (i < 0 || i >= nums.length || v[i])
+            return;
+        v[i] = true;
+        for (int k = 0; k < nums.length; k++) {
+            if (!v[k] && nums[i][k] >= 3)
+                dfs(nums, v, k);
+        }
+
+        for (int k = 0; k < nums.length; k++) {
+            if (!v[k] && nums[k][i] >= 3)
+                dfs(nums, v, k);
+        }
+
+
     }
 
     public static void main(String[] args) {
         /*int[][] nums = {{0, 4, 0}, {4, 0, 0}, {0, 0, 0}};
         int c = component(nums, 3);
-        System.out.println(c);
+        System.out.println(c);*/
 
-        int[][] nums1 = {{0, 4, 0}, {4, 0, 6}, {0, 6, 0}};
+        /*int[][] nums1 = {{0, 4, 0}, {4, 0, 6}, {0, 6, 0}};
         int c1 = component(nums1, 3);
-        System.out.println(c1);
+        System.out.println(c1);*/
 
-        int[][] nums2 = {{0}};
-        int c2 = component(nums1, 1);
+       /* int[][] nums2 = {{0}};
+        int c2 = component(nums2, 1);
         System.out.println(c2);*/
         Scanner in = new Scanner(System.in);
         while (in.hasNextInt()) {
