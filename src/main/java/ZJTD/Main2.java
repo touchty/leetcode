4ff37b00e71c481e99e47d731ccfdc564854981b
touchty <touchxyz@163.com>
2019-08-25 20:25:52 +0800

    2

--- src/main/java/ZJTD/Main2.java
+++ src/main/java/ZJTD/Main2.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Main2 {
     static int roads(int p) {
+        int mod = 1000000007;
         int[] dp = new int[p + 1];
         dp[0] = 1;
         dp[2] = 1;
@@ -13,6 +14,7 @@ public class Main2 {
 //            dp[i] = (i - 1) * dp[i - 2] * dp[i - 2];
             for (int j = 2; j <= i; j += 2) {
                 dp[i] += dp[i - j] * dp[j - 2];
+                dp[i] = dp[i] % mod;
             }
         }
         return dp[p];
