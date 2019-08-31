44e6f148659c52cb4f2067661fbc0d4f23d8264e
touchty <touchxyz@163.com>
2019-08-31 23:47:30 +0800

    区间交叉

--- src/main/java/pq/MaxInterchangeOfIntervals.java
+++ src/main/java/pq/MaxInterchangeOfIntervals.java
@@ -1,7 +1,49 @@
 package pq;
 
+import java.util.*;
+
 public class MaxInterchangeOfIntervals {
     int maxInterchange(int[][] intervals) {
         return 0;
+    }
+
+    public static List<Integer> distinct(List<int[]> intervals) {
+        TreeSet<Integer> set = new TreeSet<>();
+        for (int[] interval : intervals) {
+            set.add(interval[0]);
+            set.add(interval[1]);
+        }
+        return new ArrayList<>(set);
+    }
+
+    public static int maxInterval(List<int[]> intervals) {
+        // 提取所有x
+        List<Integer> pos = distinct(intervals);
+        List<Integer> count = new ArrayList<>();
+        Collections.sort(intervals, Comparator.comparingInt((x) -> x[0]));
+        PriorityQueue<Integer> Q = new PriorityQueue<>();
+        int index = 0;
+        for (int i = 0; i < pos.size() - 1; i++) {
+            while (index < intervals.size() &&
+                    intervals.get(index)[0] == pos.get(i)) {
+                // 右断点进堆
+                Q.add(intervals.get(index)[1]);
+                index++;
+            }
+            // 堆里面的数量就是当前线段的数量
+            count.add(Q.size());
+            // 堆里面的元素出列， 代表线段终止
+            while (!Q.isEmpty() && Q.peek() == pos.get(i).intValue()) {
+                Q.poll();
+            }
+        }
+        // 枚举所有x, 最后一个不用算了，肯定是0
+        return count.stream().max(Comparator.comparingInt((x) -> x)).get();
+    }
+
+    public static void main(String[] args) {
+        int[][] intervals = {{1,3}, {2,4}, {5,9}, {5, 10}, {6, 7}};
+        int max = maxInterval(Arrays.asList(intervals));
+        System.out.println(max);
     }
 }
