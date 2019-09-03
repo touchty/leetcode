a2d627567c03931fb7fe7cac3358a34cb32e8c28
touchty <touchxyz@163.com>
2019-09-03 18:48:24 +0800

    字节三面-字符串-子序列

--- src/main/java/test/SubSequenceFind.java
+++ src/main/java/test/SubSequenceFind.java
@@ -3,6 +3,11 @@ package test;
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+基础：给定m个不重复的字符 [a, b, c, d]，以及一个长度为n的字符串tbcacbdata，
+问能否在这个字符串中找到一个长度为m的连续子串，使得这个子串刚好由上面m个字符组成，
+顺序无所谓，返回任意满足条件的一个子串的起始位置，未找到返回-1。比如上面这个例子，acbd，3。
+ */
 public class SubSequenceFind {
     public static void main(String[] args) {
         System.out.println("Hello World!");
