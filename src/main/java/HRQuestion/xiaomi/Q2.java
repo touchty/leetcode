dcbe0c18a8b26f37bd5919aafe41658fad6c46aa
touchty <touchxyz@163.com>
2019-09-06 23:51:59 +0800

    小米-重建二叉树-coinChange

--- src/main/java/HRQuestion/xiaomi/Q2.java
+++ src/main/java/HRQuestion/xiaomi/Q2.java
@@ -1,24 +1,38 @@
 package HRQuestion.xiaomi;
 
-import java.util.Deque;
-import java.util.LinkedList;
+import tree.TreeNode;
+
+// 	536	Construct Binary Tree from String
+// 前序遍历重建二叉树
 
 public class Q2 {
-    public static class MyTreeNode {
-        public int val;
-        public MyTreeNode left;
-        public MyTreeNode right;
+    public static TreeNode str2tree(String s) {
+        if (s == null || s.length() == 0) return null;
+
+        int firstParen = s.indexOf("(");
+        int val = firstParen == -1 ? Integer.parseInt(s) : Integer.parseInt(s.substring(0, firstParen));
+        TreeNode cur = new TreeNode(val);
+
+        if (firstParen == -1) return cur;
+
+        int start = firstParen, leftParenCount = 0;
+        for (int i = start; i < s.length(); i++) {
+            if (s.charAt(i) == '(') leftParenCount++;
+            else if (s.charAt(i) == ')') leftParenCount--;
+
+            if (leftParenCount == 0 && start == firstParen) {
+                cur.left = str2tree(s.substring(start + 1, i));
+                start = i + 1;
+            } else if (leftParenCount == 0) {
+                cur.right = str2tree(s.substring(start + 1, i));
+            }
+        }
+        return cur;
     }
 
-    MyTreeNode builTree(String str) {
-        if (str == null || str.length() == 0)
-            return null;
-
-        MyTreeNode root = new MyTreeNode();
-        root.val = Integer.valueOf(str.substring(0, 1));
-        String sub = str.substring(1);
-
-        Deque<Character> stack = new LinkedList<>();
-        return null;
+    public static void main(String[] args) {
+        String tree = "1(2(3)(4()(5)))(6(7)())";
+        TreeNode root = str2tree(tree);
+        System.out.println(TreeNode.inorderTraversal_Recursive(root));
     }
 }
--- src/main/java/tree/TreeNode.java
+++ src/main/java/tree/TreeNode.java
@@ -1,5 +1,9 @@
 package tree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class TreeNode {
     public int val;
     public TreeNode left;
@@ -8,4 +12,34 @@ public class TreeNode {
     public TreeNode(int x) {
         val = x;
     }
+
+    //based on recursion
+    public static List<Integer> inorderTraversal_Recursive(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        helper(root, list);
+        return list;
+    }
+
+    public static void helper(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        helper(root.left, list);
+        list.add(root.val);
+        helper(root.right, list);
+    }
+
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> pre = new LinkedList<Integer>();
+        preHelper(root, pre);
+        return pre;
+    }
+
+    public static void preHelper(TreeNode root, List<Integer> pre) {
+        if (root == null) return;
+        pre.add(root.val);
+        preHelper(root.left, pre);
+        preHelper(root.right, pre);
+    }
+
 }
