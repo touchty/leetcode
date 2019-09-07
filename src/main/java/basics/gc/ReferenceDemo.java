3490718ac698e2f3363fd41fbc31efc7fef07829
touchty <touchxyz@163.com>
2019-09-07 20:13:12 +0800

    基础

--- src/main/java/basics/gc/ReferenceDemo.java
+++ src/main/java/basics/gc/ReferenceDemo.java
@@ -4,14 +4,22 @@ import java.lang.ref.*;
 
 public class ReferenceDemo {
     public static void main(String[] args) {
-        SoftReference<Object> soft = new SoftReference<>(new Object());
-        WeakReference<Object> weak = new WeakReference<>(new Object());
+        Object o1 = new Object();
+        SoftReference<Object> soft = new SoftReference<>(o1);
+
+        Object o2 = new Object();
+        WeakReference<Object> weak = new WeakReference<>(o2);
+
+        Object o3 = new Object();
         WeakReference<String> weakString = new WeakReference<>("abc");
-        PhantomReference<Object> phantom = new PhantomReference<>(new Object(), new ReferenceQueue<>());
+
+        Object o4 = new Object();
+        PhantomReference<Object> phantom = new PhantomReference<>(o4, new ReferenceQueue<>());
         System.gc();
         System.out.println(soft.get());
         System.out.println(weak.get());
         System.out.println(weakString.get());
         System.out.println(phantom.get());
+        System.out.println(o4.toString());
     }
 }
