604da3862a90e40b69cab478ff509239fee11604
touchty <touchxyz@163.com>
2019-09-07 10:17:48 +0800

    小米-重建二叉树-coinChange

--- src/main/java/HRQuestion/xiaomi/Q2.java
+++ src/main/java/HRQuestion/xiaomi/Q2.java
@@ -5,6 +5,41 @@ import tree.TreeNode;
 // 	536	Construct Binary Tree from String
 // 前序遍历重建二叉树
 
+/*
+
+536. Construct Binary Tree from String
+2017.07.05 02:32:37
+字数 177
+阅读 579
+You need to construct a binary tree from a string consisting of parenthesis and integers.
+
+The whole input represents a binary tree. It contains an integer followed by zero, one or two pairs of parenthesis. The integer represents the root's value and a pair of parenthesis contains a child binary tree with the same structure.
+
+You always start to construct the left child node of the parent first if it exists.
+
+Example:
+Input: "4(2(3)(1))(6(5))"
+Output: return the tree root node representing the following tree:
+4
+/
+2 6
+/ \ /
+3 1 5
+Note:
+There will only be '(', ')', '-' and '0' ~ '9' in the input string.
+An empty tree is represented by "" instead of "()".
+
+** 解题思路**
+My solution using recursion:
+For example, we have string "4(2(3)(1))(6(5))", to construct a binary tree, we can split the string to 3 parts:
+"4",
+"(2(3)(1))"
+"(6(5))"
+
+4 is the root val;
+"(2(3)(1))" is left tree;
+"(6(5))" is right tree;
+ */
 public class Q2 {
     public static TreeNode str2tree(String s) {
         if (s == null || s.length() == 0) return null;
