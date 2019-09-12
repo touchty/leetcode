31704a6746d8687db2a2cd499ca120df2bb0ec9d
touchty <touchxyz@163.com>
2019-09-12 22:56:26 +0800

    jj

--- src/main/java/test/LC207CourseSchedule.java
+++ src/main/java/test/LC207CourseSchedule.java
@@ -1,8 +1,49 @@
 package test;
 
+/*
+方法2：深度优先遍历
+算法流程（思路是通过 DFS 判断图中是否有环）：
+借助一个标志列表 flags，用于判断每个节点 i （课程）的状态：
+未被 DFS 访问：i == 0；
+已被其他节点启动的DFS访问：i == -1；
+已被当前节点启动的DFS访问：i == 1。
+对 numCourses 个节点依次执行 DFS，判断每个节点起步 DFS 是否存在环，若存在环直接返回 FalseFalse。DFS 流程；
+终止条件：
+当 flag[i] == -1，说明当前访问节点已被其他节点启动的 DFS 访问，无需再重复搜索，直接返回 TrueTrue。
+当 flag[i] == 1，说明在本轮 DFS 搜索中节点 i 被第 22 次访问，即 课程安排图有环，直接返回 FalseFalse。
+将当前访问节点 i 对应 flag[i] 置 11，即标记其被本轮 DFS 访问过；
+递归访问当前节点 i 的所有邻接节点 j，当发现环直接返回 FalseFalse；
+当前节点所有邻接节点已被遍历，并没有发现环，则将当前节点 flag 置为 -1−1 并返回 TrueTrue。
+若整个图 DFS 结束并未发现环，返回 TrueTrue。
+复杂度分析：
+时间复杂度 O(N + M)O(N+M)：遍历一个图需要访问所有节点和所有临边，NN 和 MM 分别为节点数量和临边数量；
+空间复杂度 O(N)O(N)，为建立邻接矩阵所需额外空间。
+
+作者：jyd
+链接：https://leetcode-cn.com/problems/course-schedule/solution/course-schedule-tuo-bu-pai-xu-bfsdfsliang-chong-fa/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ */
 public class LC207CourseSchedule {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[][] adjacency = new int[numCourses][numCourses];
+        int[] flags = new int[numCourses];
+        for (int[] cp : prerequisites)
+            adjacency[cp[1]][cp[0]] = 1;
+        for (int i = 0; i < numCourses; i++) {
+            if (!dfs(adjacency, flags, i)) return false;
+        }
+        return true;
+    }
 
-        return false;
+    private boolean dfs(int[][] adjacency, int[] flags, int i) {
+        if (flags[i] == 1) return false;
+        if (flags[i] == -1) return true;
+        flags[i] = 1;
+        for (int j = 0; j < adjacency.length; j++) {
+            if (adjacency[i][j] == 1 && !dfs(adjacency, flags, j)) return false;
+        }
+        flags[i] = -1;
+        return true;
     }
 }
