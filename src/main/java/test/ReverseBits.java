57c62a3b4a5e34d0786ce065f8dbe4b26862fcb5
touchty <touchxyz@163.com>
2019-09-15 16:47:38 +0800

    Q2

--- src/main/java/test/ReverseBits.java
+++ src/main/java/test/ReverseBits.java
@@ -2,9 +2,7 @@ package test;
 
 public class ReverseBits {
     static int reverse(int n) {
-        //System.out.println(Integer.toBinaryString(n));
         n = n ^ (-1);
-        //System.out.println(Integer.toBinaryString(n));
         return n;
     }
 }
