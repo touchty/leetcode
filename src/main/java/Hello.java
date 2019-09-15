14db0449e8db4bdf23ad3f3860fd7c41825a4684
touchty <touchxyz@163.com>
2019-09-15 09:39:39 +0800

    139. Word Break

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,24 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        int COUNT_BITS = Integer.SIZE - 3;
-        int CAPACITY = (1 << COUNT_BITS) - 1;
-
-// runState is stored in the high-order bits
-        int RUNNING = -1 << COUNT_BITS;
-        int SHUTDOWN = 0 << COUNT_BITS;
-        int STOP = 1 << COUNT_BITS;
-        int TIDYING = 2 << COUNT_BITS;
-        int TERMINATED = 3 << COUNT_BITS;
-        System.out.println(Integer.toBinaryString(CAPACITY));
-        System.out.println(Integer.toBinaryString(RUNNING));
-        System.out.println(Integer.toBinaryString(SHUTDOWN));
-        System.out.println(Integer.toBinaryString(STOP));
-        System.out.println(Integer.toBinaryString(TIDYING));
-        System.out.println(Integer.toBinaryString(TERMINATED));
 
     }
-    /*public static void aMethod(){
-        var name = "Java 10";
-        System.out.println(name);
-    }*/
 }
--- src/main/java/test/LC139WordBreak.java
+++ src/main/java/test/LC139WordBreak.java
@@ -1,18 +1,19 @@
 package test;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class LC139WordBreak {
-    public boolean wordBreak(String s, List<String> wordDict) {
+    public static boolean wordBreak(String s, List<String> wordDict) {
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
         Set<String> set = new HashSet<>(wordDict);
         for (int i = 0; i < s.length(); i++) {
             for (int j = 0; j <= i; j++) {
-                if (dp[j] && set.contains(s.substring(j, i+1))){
-                    dp[i+1] = true;
+                if (dp[j] && set.contains(s.substring(j, i + 1))) {
+                    dp[i + 1] = true;
                     break;
                 }
             }
@@ -21,6 +22,9 @@ public class LC139WordBreak {
     }
 
     public static void main(String[] args) {
-
+        String s = "leetcode";
+        String[] wordDict = {"leet", "code"};
+        boolean canBreak = wordBreak(s, Arrays.asList(wordDict));
+        System.out.println(canBreak);
     }
 }
