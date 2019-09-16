66ae81e3e187f4643eed9a2c18059937e5bc4441
touchty <touchxyz@163.com>
2019-09-16 08:34:22 +0800

    状态压缩

--- src/main/java/HRQuestion/wangyileihuo/Q3.java
+++ src/main/java/HRQuestion/wangyileihuo/Q3.java
@@ -1,63 +1,62 @@
 package HRQuestion.wangyileihuo;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
 
+/*
+钱老板赶工
+钱老板去国外度了个假，刚回到公司就收到了n封催促工作完成的邮件。
+每项工作都有完成截止日期的deadline，钱老板做每项工作都会花去cost天，而且不能中断。
+请你帮钱老板安排一下完成工作的顺序，以减少总的工作推迟时间
+
+输入：
+n<=20，表示工作数量
+接下来n行，每一行有两个数，表示第i项工作的deadline和cost
+
+输出：
+最小的总的工作推出时间
+标签：动态规划，状态压缩
+
+解析：
+
+由于n比较小，我们用一个int的每一位把n个任务是否已经完成，这就是状态压缩
+
+总状态数有2^n种
+
+状态转移方程：
+
+dp[x] = min\{dp[x - {i}] + sum\_cost(x) - deadline[i]\}`
+dp[x]=min{dp[x−i]+sum_cost(x)−deadline[i]}‘
+
+核心代码：
+ */
 public class Q3 {
-    static int delays(int[] currTask, List<int[]> preTasks, int start) {
-        if (preTasks.size() == 0)
-            return 0;
-
-        int sum = 0;
-
-        for (int[] task : preTasks) {
-            sum += task[1];
+    public static int minDelay(int[] deadline, int[] cost) {
+        int n = cost.length;
+        int[] sum_cost = new int[1 << n];
+        int[] dp = new int[1 << n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int x = 1; x < (1 << n); x++) {
+            sum_cost[x] = 0;
+            for (int i = 0; i < n; i++) {
+                if ((x & (1 << i)) != 0) {
+                    sum_cost[x] += cost[i];
+                }
+            }
+            for (int i = 0; i < n; i++) {
+                if ((x & (1 << i)) != 0) {
+                    int delay = sum_cost[x] - deadline[i] < 0 ? 0 : sum_cost[x] - deadline[i];
+                    dp[x] = Math.min(dp[x], dp[x - (1 << i)] + delay);
+                }
+            }
         }
-        int[] newLast = preTasks.remove(preTasks.size() - 1);
-        int delayPre = delays(newLast, preTasks, start);
-
-        if (sum <= currTask[0] - start) {
-            return delayPre;
-        } else {
-            return delayPre + sum - (currTask[0] - start);
-        }
-    }
-
-    static int delaysDP(int[][] tasks) {
-        int[][] dp = new int[tasks.length][tasks.length];
-        return 0;
-
+        return dp[dp.length - 1];
     }
 
     public static void main(String[] args) {
-        /*int[][] tasks = {{3, 3}, {8, 1}, {3, 2}};
-        Arrays.sort(tasks, ((o1, o2) -> o1[0] - o2[0]));
-        List<int[]> list = new ArrayList<>();
-        for (int[] a : tasks) {
-            list.add(a);
-        }
-        list.remove(list.size() - 1);
-        int delay = delays(tasks[0], list, 0);
-        System.out.println(delay);*/
-
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNextInt()) {
-            int N = scanner.nextInt();
-            int[][] tasks = new int[N][2];
-            for (int i = 0; i < N; i++) {
-                tasks[i][0] = scanner.nextInt();
-                tasks[i][1] = scanner.nextInt();
-            }
-            Arrays.sort(tasks, ((o1, o2) -> o1[0] - o2[0]));
-            List<int[]> list = new ArrayList<>();
-            for (int[] a : tasks) {
-                list.add(a);
-            }
-            list.remove(list.size() - 1);
-            int delay = delays(tasks[0], list, 0);
-            System.out.println(delay);
-        }
+        int[] deadline = {3, 8, 3};
+        int[] cost = {3, 1, 2};
+        int minDelay = minDelay(deadline, cost);
+        System.out.println(minDelay);
     }
 }
