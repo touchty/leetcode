c1d76b9fdbf51d65660a841bf37626b5c55b1629
touchty <touchxyz@163.com>
2019-09-17 12:06:38 +0800

    17. Letter Combinations of a Phone Number

--- src/main/java/dfs/LetterCombinationsofaPhoneNumber.java
+++ src/main/java/dfs/LetterCombinationsofaPhoneNumber.java
@@ -3,6 +3,8 @@ package dfs;
 import java.util.ArrayList;
 import java.util.List;
 
+// LC 17. Letter Combinations of a Phone Number
+// 电话盘
 public class LetterCombinationsofaPhoneNumber {
     public List<String> letterCombinations(String digits) {
         if (digits.length() == 0)
@@ -10,6 +12,7 @@ public class LetterCombinationsofaPhoneNumber {
         String[] s = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         List<String> res = new ArrayList<>();
         helper(s, res, new StringBuilder(), digits);
+        // helperII(s, res, new StringBuilder(), digits, 0);
         return res;
     }
 
@@ -26,5 +29,27 @@ public class LetterCombinationsofaPhoneNumber {
             helper(s, res, sb, digits);
             sb.deleteCharAt(sb.length() - 1);
         }
+    }
+
+    private void helperII(String[] s, List<String> res, StringBuilder sb, String digits, int pos) {
+        if (pos == digits.length()) {
+            res.add(sb.toString());
+            return;
+        }
+        if (s[pos].length() == 0) {
+            helperII(s, res, sb, digits, pos + 1);
+        } else {
+            for (int i = 0; i < s[pos].length(); i++) {
+                sb.append(s[pos].charAt(i));
+                helperII(s, res, sb, digits, pos + 1);
+                sb.deleteCharAt(sb.length() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String digits = "012";
+        LetterCombinationsofaPhoneNumber solution = new LetterCombinationsofaPhoneNumber();
+        System.out.println(solution.letterCombinations(digits));
     }
 }
