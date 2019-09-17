279f54b0987dfc73512bb86c226e9f838a27d1f7
touchty <touchxyz@163.com>
2019-09-18 00:30:50 +0800

    动态规划

--- src/main/java/LongestPalindromicSubstring.java
+++ src/main/java/LongestPalindromicSubstring.java
@@ -1,65 +1,25 @@
-import java.util.Scanner;
-
 public class LongestPalindromicSubstring {
+    // LC 5. Longest Palindromic Substring
+    // 最长回文子串， 动态规划
     public String longestPalindrome(String s) {
-        int max = 0;
-        int left = 0;
-        int right = 0;
-        for (int i = 0; i < s.length(); i++)
-            for (int j = i; j < s.length(); j++) {
-                if (isPalindromic(s, i, j)) {
-                    if (j - i + 1 > max) {
-                        max = j - i + 1;
-                        left = i;
-                        right = j;
-                        //System.out.printf("max " + max);
+        int n = s.length();
+        int maxLen = 0;
+        int start = 0;
+        boolean[][] dp = new boolean[n][n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]);
+
+                if (dp[i][j]) {
+                    if (j - i + 1 > maxLen) {
+                        maxLen = j - i + 1;
+                        start = i;
                     }
+
                 }
             }
-        //System.out.printf("left right" + left + " " + right);
-        return s.substring(left, right + 1);
-    }
-
-    boolean isPalindromic(String s, int left, int right) {
-        boolean result = true;
-        for (int i = 0; i <= (right - left) / 2; i++) {
-            if (s.charAt(left + i) != s.charAt(right - i))
-                result = false;
         }
-        return result;
-    }
-
-    private int lo = 0;
-    private int maxLen = 0;
-
-    private void extendPalindrome(String s, int j, int k) {
-        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
-            j--;
-            k++;
-        }
-        if (maxLen < k - j - 1) {
-            lo = j + 1;
-            maxLen = k - j - 1;
-        }
-    }
-
-    public String longestP(String s) {
-        for (int i = 0; i < s.length(); i++) {
-            extendPalindrome(s, i, i);
-            extendPalindrome(s, i, i + 1);
-        }
-        return s.substring(lo, maxLen);
-    }
-
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        while (in.hasNext()) {
-            String sample = in.next();
-            LongestPalindromicSubstring l = new LongestPalindromicSubstring();
-            String substr = l.longestPalindrome(sample);
-            System.out.println(substr);
-
-        }
-
+        return s.substring(start, start + maxLen);
     }
 }
