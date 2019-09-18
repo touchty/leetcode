3eb4b9d7f9fd90678eec2c00c885e9b1ef56bcb5
touchty <touchxyz@163.com>
2019-09-18 23:58:39 +0800

    q1

--- src/main/java/HRQuestion/vmware/Q1.java
+++ src/main/java/HRQuestion/vmware/Q1.java
@@ -1,61 +1,89 @@
 package HRQuestion.vmware;
 
-import java.util.Scanner;
+/*
+void calPalin(int iNum)
+{
+	int iHalfLen,iHalf,iDegree,iDigit,iResult; //iHalfLen是回文数对称左边有几位，如12521左边是3位数
+	//iHalf是回文数对称的左边一半，如12521中的125；iDegree表示几位数；
+	//iDigit表示某一几位数有多少回文数，如3位数有90个； iResult是结果
+	int i;
 
+	iDigit=9;   //赋初值为9
+
+	for (iDegree=1;;iDegree++)
+	{
+		if (iNum-iDigit<=0)  //该处=0时一定要退出！！
+		{
+			break;
+		}
+
+		iNum-=iDigit;
+		if (iDegree%2==0)  //一位数和二位数中的回文数是一样多，三位数和四位数中也一样。。。
+		{
+			iDigit*=10;
+		}
+	}
+
+	iHalfLen=(iDegree+1)/2;   //算某一回文数左边的位数，如12521左边有3位
+
+	iHalf=1;        //我觉得精华之所在
+	for (i=2;i<=iHalfLen;i++)
+	{
+		iHalf*=10;
+	}
+	iHalf+=iNum-1;
+
+	iResult=iHalf;
+	if (iDegree&1)  //如果是奇位数，那么右边比左边一半少一位
+	{
+		iHalf/=10;
+	}
+
+	while (iHalf)   //求最终结果
+	{
+		iResult=iResult*10+iHalf%10;
+		iHalf/=10;
+	}
+
+	printf("The result is :   %d\n\n",iResult);
+}
+ */
 public class Q1 {
-    public static boolean isPalindrome(String s, int di, int gao) {
-        while (di < gao)
-            if (s.charAt(di++) != s.charAt(gao--)) return false;
-        return true;
-    }
-
     public static long kP(int n, int iNum) {
         if (n == 1)
             return iNum - 1;
-        int i;
-        int HD;
-        int Mh;
-        int Md;
+        int halfLen;
+        int half;
         int result;
 
-        Mh = 1;
-        Md = n;
-        HD = (Md + 1) / 2;
+        half = 1;
+        halfLen = (n + 1) / 2;
 
 
-        for (i = 2; i <= HD; i++) {
-            Mh *= 10;
+        for (int i = 2; i <= halfLen; i++) {
+            half *= 10;
         }
-        Mh += iNum - 1;
+        half += iNum - 1;
 
-        result = Mh;
-        if ((Md & 1) != 0) {
-            Mh /= 10;
+        result = half;
+        if ((n & 1) != 0) { // 奇数
+            half /= 10;
         }
 
-        while (Mh != 0) {
-            result = result * 10 + Mh % 10;
-            Mh /= 10;
+        while (half != 0) {
+            result = result * 10 + half % 10;
+            half /= 10;
         }
 
         return result;
     }
 
     public static void main(String[] args) {
-
-        Scanner sss = new Scanner(System.in);
-        int idx = 0;
-        while (sss.hasNextInt()) {
-            int N = sss.nextInt();
-            long[] result = new long[N];
-            for (int i = 0; i < N; i++) {
-                int n = sss.nextInt();
-                int k = sss.nextInt();
-                result[idx] = kP(n, k);
-                idx++;
-            }
-            for (long l : result)
-                System.out.println(l);
-        }
+        int n1 = 1;
+        int i1 = 5;
+        System.out.println(kP(n1, i1));
+        /*int n2 = 4;
+        int i2 = 20;
+        System.out.println(kP(n2, i2));*/
     }
 }
