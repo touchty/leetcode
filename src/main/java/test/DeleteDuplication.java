5c943573e5a9c90403e3d0e1f248b47bc41fcdbf
touchty <touchxyz@163.com>
2019-10-04 21:20:19 +0800

    删除重复链表元素

---
+++ src/main/java/test/DeleteDuplication.java
@@ -0,0 +1,28 @@
+package test;
+
+import linkedList.ListNode;
+
+public class DeleteDuplication {
+    public ListNode deleteDuplication(ListNode pHead)
+    {
+        if (pHead==null || pHead.next==null){return pHead;}
+        ListNode Head = new ListNode(0);
+        Head.next = pHead;
+        ListNode pre  = Head;
+        ListNode last = Head.next;
+        while (last!=null){
+            if(last.next!=null && last.val == last.next.val){
+                // 找到最后的一个相同节点
+                while (last.next!=null && last.val == last.next.val){
+                    last = last.next;
+                }
+                pre.next = last.next;
+                last = last.next;
+            }else{
+                pre = pre.next;
+                last = last.next;
+            }
+        }
+        return Head.next;
+    }
+}
